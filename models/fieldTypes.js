c9911805f9e591591e92d7f897fcf43d635ba1ec
Weston Peters <westonrpr@yahoo.com>
2022-04-05 11:47:05 -0500

    fix import syntax error

--- models/fieldTypes.js
+++ models/fieldTypes.js
@@ -1,4 +1,11 @@
-export const requiredStr = { type: String, required: true };
-export const requiredNum = { type: Number, required: true };
-export const requiredBool = { type: Boolean, required: true };
-export const currentTime = { type: Date, default: Date.now };
+const requiredStr = { type: String, required: true, trim: true };
+const requiredNum = { type: Number, required: true, trim: true };
+const requiredBool = { type: Boolean, required: true, trim: true };
+const currentTime = { type: Date, default: Date.now };
+
+module.exports = {
+  requiredStr,
+  requiredNum,
+  requiredBool,
+  currentTime,
+};
