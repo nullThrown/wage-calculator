518792f453267f11b3df251881061ea4f471351e
Weston Peters <westonrpr@yahoo.com>
2022-04-05 15:55:13 -0500

    reorganize form layout

--- client/src/components/form/AddEntry.js
+++ client/src/components/form/AddEntry.js
@@ -1,45 +1,62 @@
-import { Box, Divider, Flex } from '@chakra-ui/react';
+import { Box, Divider, Flex, Grid, GridItem, Button } from '@chakra-ui/react';
 import SecHeading from '../typography/SecHeading';
 import TertHeading from '../typography/TertHeading';
 import NumInput from './NumInput';
-const AddEntryForm = () => {
+import RadioGrp from './Radio';
+import BrunchSwitch from './BrunchSwitch';
+
+const AddEntryForm = ({ onToggle }) => {
   return (
     <Box
       as='form'
-      w='80%'
+      maxW='640px'
+      w='100%'
+      mt='10px'
       textAlign='center'
-      border='1px solid rgba(100,100,230, .6)'
+      boxShadow='5px 5px 10px rgba(220,220,220,1.0)'
       borderRadius={10}
       p='2em'>
       <SecHeading text="Add Earning's Reports" />
 
       <Flex direction='column' align='start' m='1em 0'>
         <TertHeading text='Totals' />
-        <Divider mb='.5em' />
-        <Flex justify='space-around' w='100%'>
-          <NumInput title='Hours Worked' name='hours-worked' />
-          <NumInput title='Total Sales' name='total-sales' isRequired={true} />
-        </Flex>
+        <Divider mt='.2em' />
+        <Grid gap='21px' templateColumns='repeat(2, 1fr)' w='100%' m='.4em 0'>
+          <GridItem>
+            <NumInput title='Hours Worked' name='hours-worked' />
+          </GridItem>
+          <GridItem>
+            <NumInput
+              title='Total Sales'
+              name='total-sales'
+              isRequired={true}
+            />
+          </GridItem>
+        </Grid>
         <Divider />
       </Flex>
 
       <Flex direction='column' align='start' m='1em 0'>
         <TertHeading text='Tips' />
-        <Divider mb='.5em' />
-        <Flex justify='center'>
+        <Divider mt='.2em' />
+        <Grid gap='10px' templateColumns='repeat(3,1fr)' m='.4em 0'>
           <NumInput title='Credit Tips' name='credit-tips' />
           <NumInput title='Cash Tips' name='cash-tips' />
           <NumInput title='Tip Out' name='tip-out' />
-        </Flex>
+        </Grid>
         <Divider />
       </Flex>
       <Flex direction='column' align='start' m='1em 0'>
-        <TertHeading text='Tips' />
-        <Divider mb='.5em' />
-        <NumInput title='Credit Tips' name='credit-tips' />
-        <NumInput title='Cash Tips' name='cash-tips' />
-        <NumInput title='Tip Out' name='tip-out' />
+        <TertHeading text='Shift' />
+        <Divider mt='.2em' />
+        <Flex align='center' w='100%' justify='space-around' m='.4em 0'>
+          <RadioGrp />
+          <BrunchSwitch />
+        </Flex>
         <Divider />
+        <Button onClick={onToggle} colorScheme='green' m='2.6em auto 0'>
+          Submit Entry
+        </Button>
       </Flex>
     </Box>
   );
