5b644050710fe456ca49e46408a68daaa68fad47
Weston Peters <westonrpr@yahoo.com>
2022-04-05 16:02:47 -0500

    add addEntry form and transition functionality

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,20 @@
+import { Collapse, useDisclosure } from '@chakra-ui/react';
 import Container from '../components/base/container';
 import MainHeading from '../components/typography/MainHeading';
 import AddEntryBtn from '../components/button/AddEntry';
 import Header from '../components/base/Header';
+import AddEntryForm from '../components/form/AddEntry';
 const Home = () => {
+  const { isOpen, onToggle } = useDisclosure();
   return (
     <>
       <Header />
       <Container>
         <MainHeading text='Welcome, <username>' />
-        <AddEntryBtn />
+        <AddEntryBtn onToggle={onToggle} />
+        <Collapse in={isOpen} animateOpacity>
+          <AddEntryForm onToggle={onToggle} />
+        </Collapse>
       </Container>
     </>
   );
