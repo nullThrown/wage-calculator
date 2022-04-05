fcfe85611f3435d4dad483e986ba1af12c5eb532
Weston Peters <westonrpr@yahoo.com>
2022-04-05 11:27:25 -0500

    replace inline components with abstracted ones

--- client/src/components/form/AddEntry.js
+++ client/src/components/form/AddEntry.js
@@ -1,5 +1,7 @@
-import { Box, Heading, Divider } from '@chakra-ui/react';
-import NumInput from './Input';
+import { Box, Divider, Flex } from '@chakra-ui/react';
+import SecHeading from '../typography/SecHeading';
+import TertHeading from '../typography/TertHeading';
+import NumInput from './NumInput';
 const AddEntryForm = () => {
   return (
     <Box
@@ -9,28 +11,36 @@ const AddEntryForm = () => {
       border='1px solid rgba(100,100,230, .6)'
       borderRadius={10}
       p='2em'>
-      <Heading size='lg' fontWeight={400}>
-        Add New Earning Report
-      </Heading>
-      <Box m='1em 0'>
-        <Heading size='md' fontWeight={400} textAlign='left'>
-          Totals
-        </Heading>
+      <SecHeading text="Add Earning's Reports" />
+
+      <Flex direction='column' align='start' m='1em 0'>
+        <TertHeading text='Totals' />
         <Divider mb='.5em' />
-        <NumInput
-          title='Hours Worked'
-          name='hours-worked'
-          isRequired={true}
-          errorMsg='wage is required'
-        />
-        <NumInput
-          title='Total Sales'
-          name='total-sales'
-          isRequired={true}
-          errorMsg='tip is required'
-        />
-        <Divider mt='1em' />
-      </Box>
+        <Flex justify='space-around' w='100%'>
+          <NumInput title='Hours Worked' name='hours-worked' />
+          <NumInput title='Total Sales' name='total-sales' isRequired={true} />
+        </Flex>
+        <Divider />
+      </Flex>
+
+      <Flex direction='column' align='start' m='1em 0'>
+        <TertHeading text='Tips' />
+        <Divider mb='.5em' />
+        <Flex justify='center'>
+          <NumInput title='Credit Tips' name='credit-tips' />
+          <NumInput title='Cash Tips' name='cash-tips' />
+          <NumInput title='Tip Out' name='tip-out' />
+        </Flex>
+        <Divider />
+      </Flex>
+      <Flex direction='column' align='start' m='1em 0'>
+        <TertHeading text='Tips' />
+        <Divider mb='.5em' />
+        <NumInput title='Credit Tips' name='credit-tips' />
+        <NumInput title='Cash Tips' name='cash-tips' />
+        <NumInput title='Tip Out' name='tip-out' />
+        <Divider />
+      </Flex>
     </Box>
   );
 };
