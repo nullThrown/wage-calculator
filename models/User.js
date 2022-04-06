7ea1637b1453cf77e04d9ebfe8b1d36f4decd269
Weston Peters <westonrpr@yahoo.com>
2022-04-06 09:48:09 -0500

    add account section

--- models/User.js
+++ models/User.js
@@ -1,26 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const {
-  requiredNum,
-  requiredStr,
-  requiredBool,
-  currentTime,
-} = require('./fieldTypes');
+const { requiredNum, requiredStr, currentTime } = require('./fieldTypes');
 const userSchema = new Schema(
   {
     username: { type: String, unique: true },
+    account: {
+      hourlyWage: Number,
+      overtimeMultiplier: { type: Number, default: 1.5 },
+      position: String,
+      company: String,
+    },
     entries: [
       {
         hoursWorked: requiredNum,
         totalSales: requiredNum,
-        hourlyWage: { type: Number, default: 2.13 },
         creditTips: requiredNum,
         cashTips: requiredNum,
         tipPct: requiredNum,
         tipOut: requiredNum,
         actualTipPct: requiredNum,
         shiftType: requiredStr,
-        brunch: requiredBool,
         createdAt: currentTime,
         updatedAt: { type: Date },
       },
