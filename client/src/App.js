28a10c8e17d34219dbaa0e7f0669c7c8dd54961d
Weston Peters <westonrpr@yahoo.com>
2022-04-07 22:45:04 -0500

    remove unnecessary component import

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import Container from './components/base/container';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Test from './pages/Test';
+
 function App() {
   return (
     <Router>
