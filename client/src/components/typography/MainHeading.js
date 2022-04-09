cef8325f47479f221c7b781f200dc424d742af27
Weston Peters <westonrpr@yahoo.com>
2022-04-08 19:32:50 -0500

    alter opacity prop

--- client/src/components/typography/MainHeading.js
+++ client/src/components/typography/MainHeading.js
@@ -2,7 +2,7 @@ import { Heading } from '@chakra-ui/react';
 
 const MainHeading = ({ text }) => {
   return (
-    <Heading as='h1' size='2xl' opacity='.95' fontWeight='400'>
+    <Heading as='h1' size='2xl' opacity='.9' fontWeight='400'>
       {text}
     </Heading>
   );
