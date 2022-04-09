07bed53573899212f5aa51d70e1aaaa866b5d5b5
Weston Peters <westonrpr@yahoo.com>
2022-04-08 19:34:05 -0500

    import components for visual inspection

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -3,10 +3,12 @@ import Overview from '../components/card/Overview';
 import Day from '../components/card/Day';
 import Month from '../components/card/Month';
 import Shift from '../components/card/Shift';
+import AddEntry from '../components/form/AddEntry';
+import TimeInput from '../components/form/TimeInput';
 const Test = () => {
   return (
     <TestContainer>
-      <Shift />
+      <AddEntry />
     </TestContainer>
   );
 };
