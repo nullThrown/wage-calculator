6a4d296f885feea6c2a8efae7b19867dbc0a2fd4
Weston Peters <westonrpr@yahoo.com>
2022-04-12 15:02:18 -0500

    add new components for visual inspection

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -5,10 +5,12 @@ import Month from '../components/card/Month';
 import Shift from '../components/card/Shift';
 import AddEntry from '../components/form/AddEntry';
 import TimeInput from '../components/form/TimeInput';
+import EditEntryBtn from '../components/button/EditEntry';
+import EditEntryModal from '../components/modal/EditEntry';
 const Test = () => {
   return (
     <TestContainer>
-      <AddEntry />
+      <EditEntryModal />
     </TestContainer>
   );
 };
