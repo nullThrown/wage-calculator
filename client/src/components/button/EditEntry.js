49c022b8933660589e6ae5db873c6e6ceb8360a5
Weston Peters <westonrpr@yahoo.com>
2022-04-15 11:09:02 -0500

    pass in onClick prop

--- client/src/components/button/EditEntry.js
+++ client/src/components/button/EditEntry.js
@@ -1,8 +1,12 @@
 import { Button } from '@chakra-ui/react';
 
-const EditEntryBtn = () => {
+const EditEntryBtn = ({ onClick }) => {
   return (
-    <Button colorScheme='blackAlpha' fontWeight='500' variant='solid'>
+    <Button
+      colorScheme='blackAlpha'
+      fontWeight='500'
+      variant='solid'
+      onClick={onClick}>
       Edit Entry
     </Button>
   );
