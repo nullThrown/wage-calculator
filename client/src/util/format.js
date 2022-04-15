ab8fc0f147b4d5fa651606a389c85b9c20fe3472
Weston Peters <westonrpr@yahoo.com>
2022-04-15 11:12:48 -0500

    initial commit

---
+++ client/src/util/format.js
@@ -0,0 +1,5 @@
+export const formatDollar = (val) => '$' + val;
+export const parseDollar = (val) => val.replace(/^\$/);
+
+// export const formatPct = (val) => val + '%';
+// export const parsePct = (val) => val.replace(/^\%/);
