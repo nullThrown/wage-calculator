0fdd45dc1e2c969ff9c9bae824f28318308d1841
Weston Peters <westonrpr@yahoo.com>
2022-04-18 20:03:13 -0500

    import react query client provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 const root = createRoot(document.getElementById('root'));
-
+const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
