522088a5e6f4bb942b5bc7c30e405740ff29997e
Weston Peters <westonrpr@yahoo.com>
2022-04-22 12:43:55 -0500

    component import path change

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -1,18 +1,10 @@
 import TestContainer from '../components/base/TestContainer';
-import Overview from '../components/card/Overview';
-import Day from '../components/card/Day';
-import Month from '../components/card/Month';
-import Shift from '../components/card/Shift';
-import AddEntry from '../components/form/AddEntry';
-import TimeInput from '../components/form/TimeInput';
-import EditEntryBtn from '../components/button/EditEntry';
-import EditEntryModal from '../components/modal/EditEntry';
-import Example from '../components/Example';
+import AddEntryForm from '../components/card/AddEntryForm';
+
 const Test = () => {
   return (
     <TestContainer>
-      <Example />
-      <AddEntry />
+      <AddEntryForm />
     </TestContainer>
   );
 };
