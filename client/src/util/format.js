733c99d489adae2bd3c36af5ebaf57aba175dcd4
Weston Peters <westonrpr@yahoo.com>
2022-04-22 12:44:34 -0500

    format return value with added space char

--- client/src/util/format.js
+++ client/src/util/format.js
@@ -1,5 +1,2 @@
-export const formatDollar = (val) => '$' + val;
+export const formatDollar = (val) => '$ ' + val;
 export const parseDollar = (val) => val.replace(/^\$/);
-
-// export const formatPct = (val) => val + '%';
-// export const parsePct = (val) => val.replace(/^\%/);
