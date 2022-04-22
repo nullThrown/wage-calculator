df0711f94e0e48f2e4fb996fa11cf14305abed96
Weston Peters <westonrpr@yahoo.com>
2022-04-22 12:49:04 -0500

    add cors middleware

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongooseConnect = require('./database');
 require('dotenv').config();
+const cors = require('cors');
 const app = express();
 
 mongooseConnect();
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
