1bd11136811bfc13bd1de5467d46b52a1b9c56ff
github_username <you@domain.com>
2022-04-26 17:11:56 -0500

    remove unneeded methods

--- logic/business.js
+++ logic/business.js
@@ -1,31 +1,14 @@
 class Analytics {
+  entries = [];
+  totalHours = 0;
   constructor(entries) {
     this.entries = entries;
-  }
-
-  calcTotalHoursWorked() {
-    this.entries.reduce((acc, value) => {
+    this.totalHours = this.totalHours =  this.entries.reduce((acc, value) => {
       return acc + value.timeWorkedDec;
     }, 0);
   }
-  calcTotalTips() {
-    const totalTips = {
-      totalCash: 0,
-      totalCredit: 0,
-      totalTips: 0,
-    };
-    this.entries.forEach((entry) => {
-      totalTips.totalCash += entry.cashTips;
-      totalTips.totalCredit += entry.creditTips;
-    });
-    totalTips.totalTips = entry.cashTips + entry.creditTips;
-    return totalTips;
-  }
-  calcTotalTipOut() {
-    this.entries.reduce((acc, value) => {
-      return acc + value.tipOut;
-    }, 0);
-  }
+  
 }
 
-export default Analytics;
+
+module.exports = Analytics;
