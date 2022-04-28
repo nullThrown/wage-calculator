280748abf316fba85f7f8a16ced25f258c01d3b0
Weston Peters <westonrpr@yahoo.com>
2022-04-27 23:45:07 -0500

    initial commit

---
+++ test.js
@@ -0,0 +1,14 @@
+const arr = [
+  {
+    name: 'Punch Bowl Social',
+    active: true,
+    position: 'server',
+  },
+  {
+    name: 'Target',
+    active: true,
+    position: 'bartender',
+  },
+];
+
+console.log(JSON.stringify(arr));
