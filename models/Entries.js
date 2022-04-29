41c8008d6a5cb095d866e68eb292c8509ec8c80b
github_username <you@domain.com>
2022-04-29 14:20:08 -0500

    add virtuals for accessing totals of entries array

--- models/Entries.js
+++ models/Entries.js
@@ -8,7 +8,37 @@ const EntriesSchema = new Schema(
     user: { type: Schema.Types.ObjectId, ref: 'user' },
     entries: [EntrySchema],
   },
-  { timestamps: true }
+  { timestamps: true, toJSON: { virtuals: true } }
 );
+
+EntriesSchema.virtual('totalTimeWorked').get(function() {
+  return this.entries.reduce((acc, value) => {
+      return acc + value.timeWorkedDec;
+  },0)
+})
+EntriesSchema.virtual('totalTips').get(function() {
+  return this.entries.reduce((acc, value) => {
+      return acc + value.totalTips;
+  },0)
+})
+EntriesSchema.virtual('trueTotalTips').get(function() {
+  return this.entries.reduce((acc, value) => {
+      return acc + (value.totalTips - value.tipOut);
+  },0)
+})
+// only if applicable == true
+EntriesSchema.virtual('totalSales').get(function() {
+  return this.entries.reduce((acc, value) => {
+    if(value.totalSalesApplicable) {
+      return acc + value.totalSales;
+    }
+    else return acc;
+  },0)
+})
+EntriesSchema.virtual('totalTipOut').get(function() {
+  return this.entries.reduce((acc, value) => {
+      return acc + value.tipOut;
+  },0)
+})
 
 module.exports = mongoose.model('entries', EntriesSchema);
