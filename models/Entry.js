3ea6ac371b266cb551f390c3b04b7efec3a0d686
Weston Peters <westonrpr@yahoo.com>
2022-05-09 15:09:54 -0500

    add virtual get fn()s to produce more calculations on entry data

--- models/Entry.js
+++ models/Entry.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const {
-  requiredNum,
-  requiredStr,
-  requiredBool,
-  currentTime,
-} = require('./fieldTypes');
-// updatedAt/createdAt will most likely not be needed... we will need to make sure it works first though
+const { requiredNum, requiredStr, requiredBool } = require('./fieldTypes');
+
 const EntrySchema = new Schema(
   {
     timeWorkedDec: requiredNum,
@@ -14,12 +9,11 @@ const EntrySchema = new Schema(
     totalSalesApplicable: requiredBool,
     creditTips: requiredNum,
     cashTips: requiredNum,
-    tipPct: requiredNum,
-    actualTipPct: requiredNum,
     tipOut: requiredNum,
     shiftTime: requiredStr,
     company: requiredStr,
     position: requiredStr,
+    hourlyWage: requiredNum,
     specialEvent: requiredBool,
     shiftDate: { type: Date },
   },
@@ -29,4 +23,34 @@ const EntrySchema = new Schema(
 EntrySchema.virtual('totalTips').get(function () {
   return this.creditTips + this.cashTips;
 });
+EntrySchema.virtual('trueTips').get(function () {
+  return this.creditTips + this.cashTips - this.tipOut;
+});
+EntrySchema.virtual('totalWagesEarned').get(function () {
+  return this.timeWorkedDec * this.hourlyWage;
+});
+EntrySchema.virtual('totalEarned').get(function () {
+  return this.creditTips + this.cashTips + this.timeWorkedDec * this.hourlyWage;
+});
+EntrySchema.virtual('trueTotalEarned').get(function () {
+  return (
+    this.creditTips +
+    this.cashTips -
+    this.tipOut +
+    this.timeWorkedDec * this.hourlyWage
+  );
+});
+EntrySchema.virtual('tipPct').get(function () {
+  if (this.totalSalesApplicable) {
+    return (this.creditTips + this.cashTips) / this.totalSales;
+  } else return -1;
+});
+EntrySchema.virtual('trueTipPct').get(function () {
+  if (this.totalSalesApplicable) {
+    return (this.creditTips + this.cashTips - this.tipOut) / this.totalSales;
+  } else {
+    return -1;
+  }
+});
+
 module.exports = mongoose.model('entry', EntrySchema);
