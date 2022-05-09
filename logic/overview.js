c43c37b8c5f7ca1fc98c846de2e6838a8b5c5756
Weston Peters <westonrpr@yahoo.com>
2022-05-09 15:07:10 -0500

    remove unneeded comments & fix some logic

--- logic/overview.js
+++ logic/overview.js
@@ -1,17 +1,17 @@
+// user object can be removed
+// hourlyWage property is being stored on each entry
+
 const createOverviewData = (entries, user) => {
-  // Constants 
-    // totalTimeWorked
-    // totalTips
-    // totalSales
-    // totalTipOut 
-    const data = {
-      totalEarnedPerHour: entries.trueTotalTips / entries.totalTimeWorked + user.account.hourlyWage,
-      totalTipEarnPerHour: entries.trueTotalTips / entries.totalTimeWorked,
-      tipPct: entries.totalTips / entries.totalSales,
-      trueTipPct: entries.trueTotalTips / entries.totalSales,
-    }
-  
+  const { trueTotalTips, totalTimeWorked, totalSales, totalTips } = entries;
+  const data = {
+    totalEarnedPerHour:
+      trueTotalTips / totalTimeWorked + user.account.hourlyWage,
+    tipEarnedPerHour: trueTotalTips / totalTimeWorked,
+    tipPct: totalTips / totalSales,
+    trueTipPct: trueTotalTips / totalSales,
+  };
+
   return data;
-}
+};
 
 module.exports = createOverviewData;
