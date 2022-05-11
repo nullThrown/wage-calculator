2392ca9591771780f80bfbc90e22e3f164ce19ab
github_username <you@domain.com>
2022-05-11 18:25:53 -0500

    add virtual for creating calculated data object

--- models/Entries.js
+++ models/Entries.js
@@ -10,6 +10,53 @@ const EntriesSchema = new Schema(
   { timestamps: true, toJSON: { virtuals: true } }
 );
 
+EntriesSchema.virtual('calculatedData').get(function () {
+  // remember totals averaged includes the tipout --
+  //  we only neglect tipout when we are trying
+  // to determine tip percentage 
+  const calcData = {
+    totalTimeWorked : 0,    
+    totalEarned: 0,
+    trueTotalEarned: 0,
+    totalWages: 0,
+    creditTips: 0,
+    cashTips: 0,
+    totalTips : 0,
+    trueTotalTips : 0,
+    tipsEarnedWithSalesApp: 0,
+    tipOutWithSalesTaxApp: 0,
+    totalSales : 0,
+    totalTipOut : 0,
+    // includes tip out
+    totalPerHour: 0, 
+    tipPerHour: 0,
+    tipPct: 0,
+    trueTipPct: 0,
+
+  };
+  this.data.forEach((entry) => {
+    calcData.totalTimeWorked += entry.timeWorkedDec;
+    calcData.totalEarned += entry.totalEarned;
+    calcData.trueTotalEarned += entry.trueTotalEarned;
+    calcData.totalWages += entry.totalWages;
+    calcData.creditTips += entry.creditTips;
+    calcData.cashTips += entry.cashTips;
+    calcData.totalTips += entry.totalTips;
+    calcData.trueTotalTips += entry.trueTotalTips;
+    if(entry.totalSalesApplicable) {
+      calcData.tipsEarnedWithSalesApp += entry.totalTips;
+      calcData.tipOutWithSalesTaxApp += entry.tipOut;
+    }
+    calcData.totalSales += entry.totalSales;
+    calcData.totalTipOut += entry.tipOut
+  })
+  calcData.totalPerHour = calcData.trueTotalEarned / calcData.totalTimeWorked;
+  calcData.tipPerHour = calcData.trueTotalTips / calcData.totalTimeWorked;
+  calcData.tipPct = calcData.tipsEarnedWithSalesApp / calcData.totalSales;
+  calcData.trueTipPct = (calcData.tipsEarnedWithSalesApp - calcData.tipOutWithSalesTaxApp)   / calcData.totalSales;
+
+  return calcData;
+});
 EntriesSchema.virtual('totalTimeWorked').get(function () {
   return this.data.reduce((acc, value) => {
     return acc + value.timeWorkedDec;
@@ -37,5 +84,7 @@ EntriesSchema.virtual('totalTipOut').get(function () {
     return acc + value.tipOut;
   }, 0);
 });
+
+
 
 module.exports = mongoose.model('entries', EntriesSchema);
