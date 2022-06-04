5b82bde406eaad7be4c070811529c0c1bf85ffdb
Weston Peters <westonrpr@yahoo.com>
2022-06-04 12:39:54 -0500

    import name change

--- models/Entries.js
+++ models/Entries.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const EntrySchema = require('./Entry').schema;
+const EntrySchema = require('./Entry');
 
 const EntriesSchema = new Schema(
   {
