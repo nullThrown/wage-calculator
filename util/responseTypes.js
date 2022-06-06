4a6b0919439cfb75dcd68b31405a80e84de028b3
Weston Peters <westonrpr@yahoo.com>
2022-06-06 16:38:24 -0500

    add new response types & change prop name to 'msg'

--- util/responseTypes.js
+++ util/responseTypes.js
@@ -1,21 +1,28 @@
 // error
-const unauthenticated = { error: 'unauthenticated' };
-const resource_not_found = { error: 'resource_not_found' };
-const invalid_data = { error: 'invalid_data' };
-const invalid_credentials = { error: 'invalid_credentials' };
-const email_already_exists = { error: 'email_already_exists' };
-const server_error = { error: 'server_error' };
+const token_invalid = { msg: 'token_invalid' };
+const resource_not_found = { msg: 'resource_not_found' };
+const invalid_data = { msg: 'invalid_data' };
+const invalid_credentials = { msg: 'invalid_credentials' };
+const email_already_exists = { msg: 'email_already_exists' };
+const server_error = { msg: 'server_error' };
+
 // success
-const resource_deleted = { success: 'resource_deleted' };
-// const resource_created = 'resource_created';
-// const resource_edited = 'resource_edited';
+const token_valid = { msg: 'token_valid' };
+const password_match = { msg: 'password_match' };
+const resource_deleted = { msg: 'resource_deleted' };
+const resource_created = { msg: 'resource_created' };
+const resource_updated = { msg: 'resource_updated' };
 
 module.exports = {
-  unauthenticated,
+  token_invalid,
   resource_not_found,
   invalid_data,
   invalid_credentials,
   email_already_exists,
   server_error,
+  token_valid,
+  password_match,
   resource_deleted,
+  resource_created,
+  resource_updated,
 };
