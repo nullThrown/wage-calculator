b9cbc354bcbea6749235b97a45b9644a24921222
Weston Peters <westonrpr@yahoo.com>
2022-06-06 16:35:19 -0500

    add password field & add fields to company array

--- models/User.js
+++ models/User.js
@@ -3,12 +3,21 @@ const Schema = mongoose.Schema;
 const { requiredNum, requiredStr, currentTime } = require('./fieldTypes');
 const UserSchema = new Schema(
   {
-    username: { type: String, unique: true },
-    account: {
-      hourlyWage: Number,
-      overtimeMultiplier: { type: Number, default: 1.5 },
-      company: [{ name: String, active: Boolean, position: String }],
-    },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    companies: [
+      {
+        name: { type: String, required: true },
+        position: { type: String, required: true },
+        active: { type: Boolean, default: true },
+        hourlyWage: { type: Number, required: true },
+        overtimeMultiplier: { type: Number, required: true },
+        startDate: { type: Date, default: Date.now },
+        endDate: { type: Date },
+        isRemoved: { type: Boolean, default: false },
+      },
+    ],
     entries: { type: Schema.Types.ObjectId, ref: 'entries' },
   },
   { timestamps: true }
