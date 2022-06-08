b65bf2f2c71553e014b4055aa6d0043c06c9298e
Weston Peters <westonrpr@yahoo.com>
2022-06-08 14:02:57 -0500

    create and reorganize new routes

--- routes/entry.js
+++ routes/entry.js
@@ -2,88 +2,14 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const Entries = require('../models/Entries');
-const verifyUser = require('../middleware/auth');
+const verifyToken = require('../middleware/auth');
 const { server_error } = require('../util/responseTypes');
 
-// ROUTE GET api/entries/all
-// DESC get all entries + all data
-// ACCESS private
-router.get('/all/data/:username', verifyUser, async (req, res) => {
-  const { username } = req.params;
-  try {
-    const userId = await User.findOne({ username: username }, { _id: 1 });
-    const entries = await Entries.findOne({ user: userId });
-
-    // data by month
-    // const entries = await Entries.findOne({ user: userId });
-    // data by week
-    // const entries = await Entries.findOne({ user: userId });
-    res.status(200).json(entries);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(server_error);
-  }
-});
-
-// ROUTE GET api/entries/:month/:year/:username
-// DESC get all entries by specific month
-// ACCESS private
-router.get('/:year/:month/:username', verifyUser, async (req, res) => {
-  const { username, month, year } = req.params;
-  const startDate = new Date(year, month, 1);
-  const endDate = new Date(year, month + 1, 0);
-  try {
-    const userId = await User.findOne({ username: username }, { _id: 1 });
-    // need to create filters that selects entry subdocs between two dates
-    // const entries = await Entries.find({ user: userId });
-    res.status(200).json(entries);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(server_error);
-  }
-});
-
-// ROUTE GET api/entries/:month/:year/:day/:username
-// DESC get all entries by specific week Monday-Sunday
-// ACCESS private
-router.get('/:year/:month/:day/:username', verifyUser, async (req, res) => {
-  const { username, month, year } = req.params;
-  const startDate = new Date(year, month, 1);
-  const endDate = new Date(year, month + 1, 0);
-  try {
-    const userId = await User.findOne({ username: username }, { _id: 1 });
-    // need to create filter that queries entry subdocs by selected week
-    // a given day will be selected and entries from the preceding monday and the following
-    // sunday will be selected and returned
-    // const entries = await Entries.find({ user: userId });
-    res.status(200).json(entries);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(server_error);
-  }
-});
-
-// ROUTE GET api/entries/:id
-// DESC get single entry
-// ACCESS private
-router.get('/:id/:username', verifyUser, async (req, res) => {
-  const { username } = req.params;
-  try {
-    const userId = await User.findOne({ username: username }, { _id: 1 });
-    const entries = await Entries.findOne({ user: userId });
-    res.status(200).json(entries);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(server_error);
-  }
-});
-
 // ROUTE POST api/entries/create
-// DESC create new earning
+// DESC create new earning's entry
 // ACCESS private
-router.post('/create', verifyUser, async (req, res) => {
+router.post('/create', verifyToken, async (req, res) => {
   const {
-    username,
     timeWorkedDec,
     totalSales,
     totalSalesApplicable,
@@ -112,20 +38,92 @@ router.post('/create', verifyUser, async (req, res) => {
     specialEvent,
     shiftDate,
   };
-
   try {
-    const user = await User.findOne({ username: username });
-    let entries = await Entries.findOne({ user: user._id });
-    if (!entries) {
-      entries = new Entries({ user: user._id, data: newEntry });
-      user.entries = entries._id;
-      await user.save();
-    } else {
-      entries.data.push(newEntry);
-    }
-    await entries.save();
+    const entries = await Entries.findOneAndUpdate(
+      { user: req.user.id },
+      {
+        $push: { data: newEntry },
+      },
+      { returnOriginal: false }
+    );
 
-    res.status(201).json(entries);
+    res.status(201).json(entries.data[entries.data.length - 1]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(server_error);
+  }
+});
+
+// ROUTE POST api/entries/update
+// DESC update earning's entry
+// ACCESS private
+router.post('/update', verifyToken, async (req, res) => {});
+
+// ROUTE GET api/entries/all
+// DESC get all entries
+// ACCESS private
+router.get('/all', verifyToken, async (req, res) => {
+  try {
+    const entries = await Entries.findOne({ user: req.user.id });
+    res.status(200).json(entries);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(server_error);
+  }
+});
+
+// ROUTE GET api/entries/:month/:year/:username
+// DESC get all entries by specific month
+// ACCESS private
+router.get('/:year/:month/:username', verifyToken, async (req, res) => {
+  const { username, month, year } = req.params;
+  const startDate = new Date(year, month, 1);
+  const endDate = new Date(year, month + 1, 0);
+  try {
+    const userId = await User.findOne({ username: username }, { _id: 1 });
+    // need to create filters that selects entry subdocs between two dates
+    // const entries = await Entries.find({ user: userId });
+    res.status(200).json(entries);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(server_error);
+  }
+});
+
+// ROUTE GET api/entries/:month/:year/:day/:username
+// DESC get all entries by specific week Monday-Sunday
+// ACCESS private
+router.get('/:year/:month/:day', verifyToken, async (req, res) => {
+  const { month, year } = req.params;
+  const startDate = new Date(year, month, 1);
+  const endDate = new Date(year, month + 1, 0);
+  try {
+    const userId = await User.findOne({ username: username }, { _id: 1 });
+    // need to create filter that queries entry subdocs by selected week
+    // a given day will be selected and entries from the preceding monday and the following
+    // sunday will be selected and returned
+    // const entries = await Entries.find({ user: userId });
+    res.status(200).json(entries);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(server_error);
+  }
+});
+
+// ROUTE GET api/entries/shift
+// DESC get filtered shift data
+// ACCESS private
+router.get('/shift', verifyToken, async (req, res) => {});
+
+// ROUTE GET api/entries/:id
+// DESC get single entry
+// ACCESS private
+router.get('/:id', verifyToken, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const userId = await User.findOne({ username: username }, { _id: 1 });
+    const entries = await Entries.findOne({ user: userId });
+    res.status(200).json(entries);
   } catch (err) {
     console.log(err);
     res.status(500).json(server_error);
