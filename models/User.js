bce7a3cc727b9e314bfa72da394db8bcc1140c44
Weston Peters <westonrpr@yahoo.com>
2022-06-08 13:57:18 -0500

    designate all fields with mongoose types objects

--- models/User.js
+++ models/User.js
@@ -1,19 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const { requiredNum, requiredStr, currentTime } = require('./fieldTypes');
+const {
+  requiredNum,
+  requiredStr,
+  requiredBool,
+  currentTime,
+} = require('../util/mongooseTypes');
+
 const UserSchema = new Schema(
   {
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    username: { ...requiredStr, unique: true },
+    email: { ...requiredStr, unique: true },
+    password: requiredStr,
     companies: [
       {
-        name: { type: String, required: true },
-        position: { type: String, required: true },
-        active: { type: Boolean, default: true },
-        hourlyWage: { type: Number, required: true },
-        overtimeMultiplier: { type: Number, required: true },
-        startDate: { type: Date, default: Date.now },
+        name: requiredStr,
+        position: requiredStr,
+        active: requiredBool,
+        hourlyWage: requiredNum,
+        overtimeMultiplier: requiredNum,
+        startDate: currentTime,
         endDate: { type: Date },
         isRemoved: { type: Boolean, default: false },
       },
