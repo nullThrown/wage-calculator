2ea0ca8460fbac1de2e51376f4dfd116f2d1e899
Weston Peters <westonrpr@yahoo.com>
2022-07-30 13:25:31 -0500

    add beginnings of error handling
    The error handling required by this react-query hook will most likely be handled within the SignupForm component itself, with the mutate method call

--- client/src/features/auth/hooks/useRegisterUser.js
+++ client/src/features/auth/hooks/useRegisterUser.js
@@ -1,8 +1,16 @@
 import { useMutation } from 'react-query';
 import { registerUser } from '../api/auth';
 
-const useRegisterUser = useMutation((user) => registerUser(user));
+// const useRegisterUser = useMutation((user) => registerUser(user));
 // useRegisterUSer instance should handle the UI logic
 // error/success states, redirects etc.
+
+function useRegisterUser(user) {
+  return useMutation(() => registerUser(user), {
+    onError: (error, variables, context) => {
+      // console.log(error);
+    },
+  });
+}
 
 export default useRegisterUser;
