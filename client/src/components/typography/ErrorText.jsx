e0b138a3fed07148b70a0a7c7a6e703b82ddfcd0
Weston Peters <westonrpr@yahoo.com>
2022-07-30 13:11:01 -0500

    styling modifications & change include children prop for text value

--- client/src/components/typography/ErrorText.jsx
+++ client/src/components/typography/ErrorText.jsx
@@ -2,10 +2,15 @@ import { Text } from '@chakra-ui/react';
 
 import React from 'react';
 
-export const ErrorText = ({ text, textAlign }) => {
+export const ErrorText = ({ children, textAlign, m }) => {
   return (
-    <Text fontSize='lg' color='red.700' textAlign={textAlign}>
-      {text}
+    <Text
+      fontSize='lg'
+      color='red.800'
+      m={m}
+      fontWeight='400'
+      textAlign={textAlign}>
+      {children}
     </Text>
   );
 };
