3dee79793b297e6507f2cb68ca7e0f1342aa937b
Weston Peters <westonrpr@yahoo.com>
2022-07-30 13:25:59 -0500

    remove a console.log

--- client/src/features/entries/components/DateBox.jsx
+++ client/src/features/entries/components/DateBox.jsx
@@ -2,7 +2,6 @@ import { Box } from '@chakra-ui/react';
 import EntryDisplay from 'features/entries/components/EntryDisplay';
 
 const DateBox = ({ day, entries }) => {
-  console.log(entries);
   return (
     <Box w='120px' h='120px' border='1px solid gray' borderRadius='3px'>
       {day}
