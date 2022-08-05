db21549521cae313e064bd500ed26bd18f67902d
Weston Peters <westonrpr@yahoo.com>
2022-08-04 20:35:04 -0500

    add routes for signup process & redirects for public pages

--- client/src/routes/protected.js
+++ client/src/routes/protected.js
@@ -1,5 +1,8 @@
 import Home from 'pages/protected/Home';
 import Account from 'pages/protected/Account';
+import SignupSuccess from 'pages/protected/SignupSuccess';
+import ProfileCreation from 'pages/protected/ProfileCreation';
+import { Navigate } from 'react-router-dom';
 const protectedRoutes = [
   {
     path: '/home',
@@ -8,6 +11,26 @@ const protectedRoutes = [
   {
     path: '/account',
     element: <Account />,
+  },
+  {
+    path: '/signup-success',
+    element: <SignupSuccess />,
+  },
+  {
+    path: '/create-profile',
+    element: <ProfileCreation />,
+  },
+  {
+    path: '/',
+    element: <Navigate to='/home' />,
+  },
+  {
+    path: '/login',
+    element: <Navigate to='/home' />,
+  },
+  {
+    path: '/signup',
+    element: <Navigate to='/home' />,
   },
 ];
 
