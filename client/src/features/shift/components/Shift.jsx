35ba6e2ea04195e52bca41e76f1db61ff7b66797
Weston Peters <westonrpr@yahoo.com>
2022-08-04 20:30:34 -0500

    imported component variable name change

--- client/src/features/shift/components/Shift.jsx
+++ client/src/features/shift/components/Shift.jsx
@@ -1,15 +1,15 @@
 import { Flex } from '@chakra-ui/react';
-import Card from 'components/base/Card';
+import LargeCard from 'components/card/LargeCard';
 import QuatHeading from 'components/typography/QuatHeading';
 import TertHeading from 'components/typography/TertHeading';
 const Shift = () => {
   return (
-    <Card as='section'>
+    <LargeCard as='section'>
       <TertHeading text='Shift' textAlign='center' />
       <Flex direction='column'>
         <QuatHeading text='Totals' />
       </Flex>
-    </Card>
+    </LargeCard>
   );
 };
 export default Shift;
