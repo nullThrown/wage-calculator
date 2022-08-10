57cd49c32062c13acffa70aba0f066922a85f71f
Weston Peters <westonrpr@yahoo.com>
2022-08-09 21:59:23 -0500

    add new comments & add local state to select component

--- client/src/features/companySelect/components/CompanySelect.jsx
+++ client/src/features/companySelect/components/CompanySelect.jsx
@@ -1,17 +1,32 @@
 import { Select, Box } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CompanyDisplay from './CompanyDisplay';
-const CompanySelect = () => {
-  const [filters, setFilters] = useState([
+const CompanySelect = ({ filter, setFilter }) => {
+  // filter companies will need to be supplemented from user.companies
+  // pull user.companies from user cache
+  // map companies to name only -- place those into filter options
+  // if single company is selected -- match name with user.companies
+  // place user.company into companyDisplay
+  const [filterOptions, setFilterOptions] = useState([
     'all',
     'active',
     'Punch Bowl Social',
     'Iron Cactus',
   ]);
+  //supplement filterOptions
+  // useEffect(() => {
+  //   setFilterOptions([...filterOptions, user.Companies]);
+  // }, [user]);
+
+  const onFilterChange = (e) => setFilter(e.target.value);
   return (
     <Box>
-      <Select placeholder='select filter' variant='filled'>
-        {filters.map((filter) => {
+      <Select
+        placeholder='select filter'
+        variant='filled'
+        value={filter}
+        onChange={onFilterChange}>
+        {filterOptions.map((filter) => {
           return <option value={filter}>{filter}</option>;
         })}
       </Select>
