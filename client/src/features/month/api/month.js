c22859a448c971e2390582bd9100b43ca816f0d1
Weston Peters <westonrpr@yahoo.com>
2022-08-29 19:49:10 -0500

    return data property from axios response objects for all API calls

--- client/src/features/month/api/month.js
+++ client/src/features/month/api/month.js
@@ -2,9 +2,12 @@ import axios from 'config/axios';
 
 export const getEntryDataByMonth = async (year, month, filter) => {
   try {
-    const res = await axios.get(`/entries/month/${year}/${month}/${filter}`);
-    return res;
+    const { data } = await axios.get(
+      `/entries/month/${year}/${month}/${filter}`
+    );
+    return data;
   } catch (err) {
+    // add error handler
     console.log(err);
     return err;
   }
