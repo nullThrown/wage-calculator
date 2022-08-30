0fb8a66e4eed65925f6999e763f64e3af2c3190e
Weston Peters <westonrpr@yahoo.com>
2022-08-29 19:49:54 -0500

    add react-query useQuery hook & display data in component

--- client/src/features/month/components/Month.jsx
+++ client/src/features/month/components/Month.jsx
@@ -4,7 +4,17 @@ import QuatHeading from 'components/typography/QuatHeading';
 import TertHeading from 'components/typography/TertHeading';
 import StatRow from 'components/data/StatRow';
 import SimpleStat from 'components/data/SimpleStat';
-const Month = () => {
+import { getEntryDataByMonth } from '../api/month';
+import { useQuery } from 'react-query';
+import { useEffect } from 'react';
+import { getMonth } from 'date-fns';
+
+const Month = ({ filter }) => {
+  const { isLoading, isError, data } = useQuery(
+    ['entries', 'data', 2022, 7, filter],
+    () => getEntryDataByMonth(2022, 8, filter)
+  );
+
   return (
     <LargeCard as='section'>
       <TertHeading text='Month' textAlign='center' />
@@ -13,12 +23,15 @@ const Month = () => {
         <StatRow>
           <SimpleStat
             title='Total Earned'
-            amount='2465.96'
+            amount={data.trueTotalEarned.toFixed(2)}
             helpText='wages and tips'
           />
-          <SimpleStat title='Wages Earned' amount='1232.34' />
-          <SimpleStat title='Credit Tips' amount='1000.34' />
-          <SimpleStat title='Cash Tips' amount='232.34' />
+          <SimpleStat
+            title='Wages Earned'
+            amount={data.totalWages.toFixed(2)}
+          />
+          <SimpleStat title='Credit Tips' amount={data.creditTips.toFixed(2)} />
+          <SimpleStat title='Cash Tips' amount={data.cashTips.toFixed(2)} />
         </StatRow>
       </Flex>
       <Flex direction='column'>
@@ -27,12 +40,20 @@ const Month = () => {
           <SimpleStat
             title='Total Avg. Per Hour'
             symbolBefore='$'
-            amount='21.56'
+            amount={data.totalPerHour.toFixed(2)}
             helpText='wages and tips'
           />
-          <SimpleStat title='Avg. tip per hour' symbol='$' amount='7.23' />
-          <SimpleStat title='Avg. tip percentage' symbol='%' amount='19' />
-          <SimpleStat title='Cash Tips' amount='232.34' />
+          <SimpleStat
+            title='Avg. tip per hour'
+            symbol='$'
+            amount={data.tipPerHour.toFixed(2)}
+          />
+          <SimpleStat
+            title='Avg. tip percentage'
+            symbolAfter='%'
+            amount={(data.tipPct * 100).toFixed(2)}
+          />
+          <SimpleStat title='Cash Tips' />
         </StatRow>
       </Flex>
     </LargeCard>
