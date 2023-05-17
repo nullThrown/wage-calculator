f9448ac704d95740d11cffb18e7f86afd5e1f32d
Weston Peters <westonrpr@yahoo.com>
2023-05-16 21:18:22 -0500

    store jwt token in local storage upon successful registration
    user automatically signs in upon a successful registration.

--- client/src/features/auth/hooks/useRegisterUser.js
+++ client/src/features/auth/hooks/useRegisterUser.js
@@ -1,5 +1,6 @@
 import { useMutation } from 'react-query';
 import { registerUser } from '../api/auth';
+import storage from '../../../util/storage';
 
 // const useRegisterUser = useMutation((user) => registerUser(user));
 // useRegisterUSer instance should handle the UI logic
@@ -9,6 +10,9 @@ function useRegisterUser(user) {
   return useMutation(() => registerUser(user), {
     onError: (error, variables, context) => {
       // console.log(error);
+    },
+    onSuccess: (data, variables, context) => {
+      storage.setToken(data.data.token);
     },
   });
 }
