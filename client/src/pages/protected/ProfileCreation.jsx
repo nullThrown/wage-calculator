fe1397ed7666ebf7ab300c00cd5db5218cef057a
Weston Peters <westonrpr@yahoo.com>
2023-05-22 18:15:41 -0500

    [move] components for inputs and buttons into separate files
    *inputs & buttons moved into client/src/features/user/components/registration/addCompany

--- client/src/pages/protected/ProfileCreation.jsx
+++ client/src/pages/protected/ProfileCreation.jsx
@@ -1,32 +1,30 @@
 import { useState } from 'react';
-import CenterContainer from 'components/base/CenterContainer';
-import MainHeading from 'components/typography/MainHeading';
-import SmallCard from 'components/card/SmallCard';
+
 import {
   Button,
-  Text,
+  ButtonGroup,
   Box,
   Flex,
   useDisclosure,
-  Input,
-  FormLabel,
-  FormControl,
-  FormHelperText,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  NumberIncrementStepper,
-  NumberDecrementStepper,
   useToast,
-  FormErrorMessage,
 } from '@chakra-ui/react';
+import ErrorText from 'components/typography/ErrorText';
+import Header from 'features/user/components/registration/Header';
+import CenterContainer from 'components/base/CenterContainer';
+import SmallCard from 'components/card/SmallCard';
+import NameInput from 'features/user/components/registration/addCompany/NameInput';
+import PositionInput from 'features/user/components/registration/addCompany/PositionInput';
+import WageInput from 'features/user/components/registration/addCompany/WageInput';
+import OvertimeInput from 'features/user/components/registration/addCompany/OvertimeInput';
+import AddCompanyBtn from 'features/user/components/registration/addCompany/AddCompanyBtn';
+import EditCompanyBtn from 'features/user/components/registration/addCompany/EditCompanyBtn';
+import CancelEditBtn from 'features/user/components/registration/addCompany/CancelEditBtn';
 import { useNavigate } from 'react-router-dom';
 import useAddCompany from 'features/user/hooks/useAddCompany';
-import ErrorText from 'components/typography/ErrorText';
 import useAddCompanyVal from 'features/auth/hooks/useAddCompanyVal';
 import useUpdateCompany from 'features/user/hooks/useUpdateCompany';
-import CompanyDisplay from 'features/user/components/addCompany/CompanyDisplay';
-import FinishInfoModal from 'features/user/components/addCompany/FinishInfoModal';
+import CompanyDisplay from 'features/user/components/registration/addCompany/CompanyDisplay';
+import FinishInfoModal from 'features/user/components/registration/addCompany/FinishInfoModal';
 
 // undefined value is used when edit mode is set == true
 const initialCompanyState = {
@@ -34,41 +32,39 @@ const initialCompanyState = {
   position: '',
   hourlyWage: 0,
   overtimeMultiplier: 1,
-  _id: undefined,
+  _id: null,
 };
+
 const ProfileCreation = () => {
-  // rename to formData
-  const [newCompany, setNewCompany] = useState(initialCompanyState);
+  const [formData, setFormData] = useState(initialCompanyState);
   const [companyList, setCompanyList] = useState([]);
-  //renamed to isValidationError
-  const [displayErrors, setDisplayErrors] = useState(false);
+  const [isValidationError, setisValidationError] = useState(false);
   const [isEditMode, setIsEditMode] = useState(false);
 
-  const { isOpen, onToggle } = useDisclosure();
   const modalHook = useDisclosure();
   const toast = useToast();
   const navigate = useNavigate();
   const addCompany = useAddCompany();
   const updateCompany = useUpdateCompany();
-  const { isNameError, isPositionError } = useAddCompanyVal(newCompany);
+  const { isNameError, isPositionError } = useAddCompanyVal(formData);
 
   const handleNumberChange = (value, name) => {
-    setNewCompany({ ...newCompany, [name]: value });
+    setFormData({ ...formData, [name]: value });
   };
   const handleTextChange = (e) => {
     const { name, value } = e.target;
-    setNewCompany({ ...newCompany, [name]: value });
+    setFormData({ ...formData, [name]: value });
   };
 
   const handleAddCompany = (e) => {
     e.preventDefault();
 
     if (isNameError || isPositionError) {
-      setDisplayErrors(true);
+      setisValidationError(true);
     } else {
       // should local state be updated with a mutation?
       // should local state not be used at all in lieu of a proper query?
-      addCompany.mutate(newCompany, {
+      addCompany.mutate(formData, {
         onSuccess: (data, variables, context) => {
           setCompanyList(data.data);
           toast({
@@ -78,13 +74,13 @@ const ProfileCreation = () => {
             isClosable: true,
             position: 'top',
           });
-          setNewCompany(initialCompanyState);
+          setFormData(initialCompanyState);
         },
         onError: (error, variables, context) => {
           console.log(error.message);
         },
       });
-      setDisplayErrors(false);
+      setisValidationError(false);
     }
   };
 
@@ -102,7 +98,7 @@ const ProfileCreation = () => {
           position: 'top',
         });
         setIsEditMode(false);
-        setNewCompany(initialCompanyState);
+        setFormData(initialCompanyState);
       },
       onError: (error, variables, context) => {
         console.log(error);
@@ -112,12 +108,12 @@ const ProfileCreation = () => {
 
   const handleSetEditMode = (companyToBeUpdated) => {
     setIsEditMode(true);
-    setNewCompany(companyToBeUpdated);
+    setFormData(companyToBeUpdated);
   };
 
   const handleCancelEditMode = () => {
     setIsEditMode(false);
-    setNewCompany(initialCompanyState);
+    setFormData(initialCompanyState);
   };
 
   const handleFinish = (e) => {
@@ -135,13 +131,7 @@ const ProfileCreation = () => {
       <Box as='main'>
         <CenterContainer bg='grey' className='test'>
           <SmallCard>
-            {/* separate this into component called header */}
-            <MainHeading textAlign='center'>Add Company</MainHeading>
-            <Text mt='15px' textAlign='center'>
-              Add a company to your profile to begin creating earning's reports
-              towards.
-            </Text>
-            {/*  */}
+            <Header />
             <CompanyDisplay
               companyList={companyList}
               setCompanyList={setCompanyList}
@@ -152,74 +142,30 @@ const ProfileCreation = () => {
               as='form'
               m='30px 0 0'
               boxShadow='5px 5px 10px rgb(220,220,220)'>
-              {/* separate this into component called NameInput */}
-              <FormControl isInvalid={isNameError & displayErrors}>
-                <FormLabel htmlFor='name'>Company Name</FormLabel>
-                <Input
-                  name='name'
-                  id='name'
-                  value={newCompany.name}
-                  onChange={handleTextChange}></Input>
-                <FormErrorMessage>Please add a company name.</FormErrorMessage>
-              </FormControl>
-              {/*  */}
-              {/* separate this into component called PositionInput */}
-              <FormControl isInvalid={isPositionError & displayErrors}>
-                <FormLabel htmlFor='position'>Position</FormLabel>
-                <Input
-                  name='position'
-                  id='position'
-                  value={newCompany.position}
-                  onChange={handleTextChange}></Input>
-                <FormErrorMessage>Please add a position.</FormErrorMessage>
-                <FormHelperText>i.e., server, bartender, etc.</FormHelperText>
-              </FormControl>
-              {/*  */}
-              {/* separate this into component called WageInput */}
-              <FormControl>
-                <FormLabel htmlFor='hourlywage'>Hourly Wage</FormLabel>
-                <NumberInput
-                  name='hourlyWage'
-                  id='hourlyWage'
-                  min={0}
-                  step={1}
-                  precision={2}
-                  value={newCompany.hourlyWage}
-                  onChange={(value) => handleNumberChange(value, 'hourlyWage')}>
-                  <NumberInputField />
-                  <NumberInputStepper>
-                    <NumberIncrementStepper />
-                    <NumberDecrementStepper />
-                  </NumberInputStepper>
-                </NumberInput>
-              </FormControl>
-              {/*  */}
-              {/* separate this into component called OvertimeInput */}
-              <FormControl>
-                <FormLabel htmlFor='overtimeMultiplier'>
-                  Overtime Multiplier
-                </FormLabel>
-                <NumberInput
-                  name='overtimeMultiplier'
-                  id='overtimeMultiplier'
-                  min={1}
-                  step={0.1}
-                  precision={2}
-                  value={newCompany.overtimeMultiplier}
-                  onChange={(value) =>
-                    handleNumberChange(value, 'overtimeMultiplier')
-                  }>
-                  <NumberInputField />
-                  <NumberInputStepper>
-                    <NumberIncrementStepper />
-                    <NumberDecrementStepper />
-                  </NumberInputStepper>
-                </NumberInput>
-                <FormHelperText>
-                  If hourly wage is increased by 50% set to 1.5.
-                </FormHelperText>
-              </FormControl>
-              {/*  */}
+              <NameInput
+                isNameError={isNameError}
+                isValidationError={isValidationError}
+                formData={formData}
+                handleTextChange={handleTextChange}
+              />
+
+              <PositionInput
+                isPositionError={isPositionError}
+                isValidationError={isValidationError}
+                formData={formData}
+                handleTextChange={handleTextChange}
+              />
+
+              <WageInput
+                formData={formData}
+                handleNumberChange={handleNumberChange}
+              />
+
+              <OvertimeInput
+                formData={formData}
+                handleNumberChange={handleNumberChange}
+              />
+
               {/* updateCompany.error should check this as well */}
               {(addCompany.error?.message === 'connection_error' ||
                 addCompany.error?.message === 'server_error') && (
@@ -228,38 +174,24 @@ const ProfileCreation = () => {
                   Something went wrong :) Please Try again
                 </ErrorText>
               )}
-              {/* separate buttons into components/buttons folder  */}
+
               <Flex justifyContent='center'>
-                {!isEditMode ? (
-                  <Button
-                    type='submit'
-                    colorScheme='green'
-                    textAlign='center'
-                    m='30px auto 15px'
-                    isLoading={addCompany.isLoading ? true : false}
-                    loadingText='Adding'
-                    onClick={handleAddCompany}>
-                    Add Company
-                  </Button>
+                {isEditMode ? (
+                  <ButtonGroup gap='3'>
+                    <EditCompanyBtn
+                      updateCompany={updateCompany}
+                      handleUpdateCompany={handleUpdateCompany}
+                      formData={formData}
+                    />
+                    <CancelEditBtn
+                      handleCancelEditMode={handleCancelEditMode}
+                    />
+                  </ButtonGroup>
                 ) : (
-                  <Button
-                    type='submit'
-                    colorScheme='yellow'
-                    textAlign='center'
-                    m='30px auto 15px'
-                    isLoading={updateCompany.isLoading ? true : false}
-                    loadingText='Updating'
-                    onClick={(e) => handleUpdateCompany(e, newCompany)}>
-                    Edit Company
-                  </Button>
-                )}
-                {isEditMode && (
-                  <Button
-                    type='button'
-                    colorScheme='red'
-                    onClick={handleCancelEditMode}>
-                    Cancel
-                  </Button>
+                  <AddCompanyBtn
+                    addCompany={addCompany}
+                    handleAddCompany={handleAddCompany}
+                  />
                 )}
               </Flex>
             </Box>
