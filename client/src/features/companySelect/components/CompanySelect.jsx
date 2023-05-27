6532b872cd501d4aca876f0337006c34ed2bb1dd
Weston Peters <westonrpr@yahoo.com>
2023-05-27 18:35:21 -0500

    [feat] add get companies hook
    *Add hook to get company list from user model. *Remove old attempt to get company list. *Hard code first two values of company select 'all' & 'active'.

--- client/src/features/companySelect/components/CompanySelect.jsx
+++ client/src/features/companySelect/components/CompanySelect.jsx
@@ -1,41 +1,34 @@
 import { Select, Box } from '@chakra-ui/react';
-import QuatHeading from 'components/typography/QuatHeading';
 import TertHeading from 'components/typography/TertHeading';
-import axios from 'config/axios';
-import { useEffect, useState } from 'react';
 import CompanyDisplay from './CompanyDisplay';
-import { useQuery, queryCache } from 'react-query';
-import { getUser } from 'features/auth/api/auth';
-const CompanySelect = ({ filter, setFilter }) => {
-  const [filterOptions, setFilterOptions] = useState(['all', 'active']);
-  const user = useQuery(['user'], getUser, {
-    initialData: () => {
-      return queryCache.getQueryData('user');
-    },
-  });
+import useGetCompanies from '../hooks/useGetCompanies';
 
-  useEffect(() => {
-    setFilterOptions([
-      ...filterOptions,
-      user?.data?.companies.map((comp) => comp.name),
-    ]);
-  }, [user.data]);
-  const onFilterChange = (e) => setFilter(e.target.value);
+const CompanySelect = ({ filter, setFilter }) => {
+  const { isLoading, isError, companyList } = useGetCompanies();
+
+  const handleFilterChange = (e) => setFilter(e.target.value);
+
   return (
     <Box>
       <Box m='1em 0'>
         <TertHeading textAlign='center'>Filter Companies</TertHeading>
       </Box>
-      <Select variant='filled' value={filter} onChange={onFilterChange}>
-        {filterOptions.map((filter, i) => {
+      <Select variant='filled' value={filter} onChange={handleFilterChange}>
+        <option key='1' value='all'>
+          All
+        </option>
+        <option key='2' value='active'>
+          Active
+        </option>
+        {companyList.map((company) => {
           return (
-            <option key={i} value={filter}>
-              {filter}
+            <option key={company._id} value={company.name}>
+              {company.name}
             </option>
           );
         })}
       </Select>
-      <CompanyDisplay />
+      <CompanyDisplay filter={filter} />
     </Box>
   );
 };
