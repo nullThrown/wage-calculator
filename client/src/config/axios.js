67022539ff6c1aa2a0e898f86300c0e18e67e551
Weston Peters <westonrpr@yahoo.com>
2023-05-28 09:55:18 -0500

    [feat] add a date transformer to all request bodies
    Date transformer is currently commented out. It may have value in the future but a transformer on all request bodies may break things.

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,9 +1,30 @@
-import Axios from 'axios';
+import Axios, { AxiosRequestTransformer } from 'axios';
 import storage from '../util/storage';
 
+// recursive fn that checks every value of request body
+// if value is Date it converts it into a timestamp with Date.now()
+// this prevents the date from being serialized in a UTC time
+
+// const dateTransformer = (data) => {
+//   if (data instanceof Date) {
+//     data = Date.now();
+//     console.log(data);
+//     return data;
+//   }
+//   if (Array.isArray(data)) {
+//     return data.map((val) => dateTransformer(val));
+//   }
+//   if (typeof data === 'object' && data !== null) {
+//     return Object.fromEntries(
+//       Object.entries(data).map(([key, val]) => [key, dateTransformer(val)])
+//     );
+//   }
+//   return data;
+// };
 const axios = Axios.create({
   baseURL: 'http://localhost:4000/api',
   headers: { x_auth_token: storage.getToken() },
+  // transformRequest: [dateTransformer].concat(Axios.defaults.transformRequest),
 });
 
 export default axios;
