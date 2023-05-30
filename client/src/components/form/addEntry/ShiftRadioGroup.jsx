8034e4365ff72509d31a7d56836635b00a8da2a3
Weston Peters <westonrpr@yahoo.com>
2023-05-30 14:56:52 -0500

    [fix] layout issues & [add] label component

--- client/src/components/form/addEntry/ShiftRadioGroup.jsx
+++ client/src/components/form/addEntry/ShiftRadioGroup.jsx
@@ -1,21 +1,25 @@
-import { Radio, RadioGroup, Stack } from '@chakra-ui/react';
+import { Radio, RadioGroup, Stack, Flex, FormLabel } from '@chakra-ui/react';
 
 const ShiftRadioGroup = ({ onChange, value }) => {
   return (
-    <RadioGroup
-      defaultValue='morning'
-      name='shiftTime'
-      onChange={onChange}
-      value={value}>
-      <Stack direction='row' spacing={4}>
-        <Radio size='md' value='morning' colorScheme='yellow'>
-          Morning
-        </Radio>
-        <Radio size='md' value='night'>
-          Night
-        </Radio>
-      </Stack>
-    </RadioGroup>
+    <Flex flexDirection='column' ml='2em' mt='0' p='0'>
+      <FormLabel opacity='.85'>Shift Time</FormLabel>
+      <RadioGroup
+        mt='.4em'
+        defaultValue='morning'
+        name='shiftTime'
+        onChange={onChange}
+        value={value}>
+        <Stack direction='row' spacing={4}>
+          <Radio size='md' value='morning' colorScheme='yellow'>
+            Morning
+          </Radio>
+          <Radio size='md' value='night'>
+            Night
+          </Radio>
+        </Stack>
+      </RadioGroup>
+    </Flex>
   );
 };
 
