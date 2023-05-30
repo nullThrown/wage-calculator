3afa8bd5d4a6814a5c7064f4e539ce4c1728fbcd
Weston Peters <westonrpr@yahoo.com>
2023-05-30 14:54:18 -0500

    [fix] layout issues & [add] label component

--- client/src/components/form/addEntry/CompanySelect.jsx
+++ client/src/components/form/addEntry/CompanySelect.jsx
@@ -1,15 +1,18 @@
-import { Select } from '@chakra-ui/react';
+import { Select, Flex, FormLabel } from '@chakra-ui/react';
 export const CompanySelect = ({ onChange, companyId, companyList }) => {
   return (
-    <Select onChange={onChange} value={companyId} name='company'>
-      {companyList.map((company) => {
-        return (
-          <option key={company._id} value={company._id}>
-            {company.name}
-          </option>
-        );
-      })}
-    </Select>
+    <Flex flexDirection='column' mr='2em'>
+      <FormLabel opacity='.85'>Company</FormLabel>
+      <Select mt='.4em' onChange={onChange} value={companyId} name='company'>
+        {companyList.map((company) => {
+          return (
+            <option key={company._id} value={company._id}>
+              {company.name}
+            </option>
+          );
+        })}
+      </Select>
+    </Flex>
   );
 };
 export default CompanySelect;
