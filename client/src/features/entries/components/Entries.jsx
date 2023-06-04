0300af1f5b759bdc91bed236bb33eeb038c82de8
Weston Peters <westonrpr@yahoo.com>
2023-06-03 19:59:04 -0500

    [fix] date variable format & [feat] add get entries by week hook

--- client/src/features/entries/components/Entries.jsx
+++ client/src/features/entries/components/Entries.jsx
@@ -1,19 +1,28 @@
 import Week from 'features/entries/components/Week';
 import Day from 'features/entries/components/Day';
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import useGetEntriesByWeek from '../hooks/useGetWeeklyEntries';
 const Entries = ({ filter }) => {
   const [date, getDate] = useState(
-    new Date().toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-    })
+    new Date()
+      .toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .replace(/\//g, '-')
   );
+
+  const { isLoading, isError, data } = useGetEntriesByWeek(date, filter);
   return (
     <>
-      <Day filter={filter} date={date} />
-      <Week filter={filter} date={date} />
+      <Day
+        filter={filter}
+        date={date}
+        isLoading={isLoading}
+        isError={isError}
+      />
+      <Week filter={filter} entries={data} date={date} />
     </>
   );
 };
