0e34cd09bf07f614228cb54d999abc3d278af823
Weston Peters <westonrpr@yahoo.com>
2023-06-13 21:34:52 -0500

    [move] files moved into /components/link

--- client/src/components/button/Login.jsx
+++
@@ -1,6 +0,0 @@
-import { Button } from '@chakra-ui/react';
-const Login = () => {
-  return <Button colorScheme='blue'>Login</Button>;
-};
-
-export default Login;
--- client/src/components/button/Signup.jsx
+++
@@ -1,6 +0,0 @@
-import { Button } from '@chakra-ui/react';
-const Signup = () => {
-  return <Button colorScheme='teal'>Signup</Button>;
-};
-
-export default Signup;
