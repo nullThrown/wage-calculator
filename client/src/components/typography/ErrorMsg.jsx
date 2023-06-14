c5a0985ab42c4c09c435f5f8e2fd119fc6daa30f
Weston Peters <westonrpr@yahoo.com>
2023-06-13 21:47:26 -0500

    [delete] duplicate error message component

--- client/src/components/typography/ErrorMsg.jsx
+++
@@ -1,9 +0,0 @@
-import { Text } from '@chakra-ui/react';
-const ErrorMsg = ({ children }) => {
-  return (
-    <Text fontSize='md' color='red.600'>
-      {children}
-    </Text>
-  );
-};
-export default ErrorMsg;
