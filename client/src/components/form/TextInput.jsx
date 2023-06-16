6c923be59cd878ba82ae913934877f4c97e9ae07
Weston Peters <westonrpr@yahoo.com>
2023-06-15 21:12:51 -0500

    [feat] create the text input component

--- client/src/components/form/TextInput.jsx
+++ client/src/components/form/TextInput.jsx
@@ -1,5 +1,37 @@
-const TextInput = () => {
-  return <div>TextInput</div>;
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+} from '@chakra-ui/react';
+const TextInput = ({
+  title,
+  name,
+  type,
+  value,
+  onChange,
+  helperText,
+  isInvalid,
+  isDisabled,
+  errorMsg,
+}) => {
+  return (
+    <FormControl isInvalid={isInvalid} isDisabled={isDisabled}>
+      <FormLabel htmlFor={name} opacity='.85'>
+        {title}
+      </FormLabel>
+      <Input
+        type={type ? type : 'text'}
+        id={name}
+        value={value}
+        name={name}
+        onChange={(e) => onChange(e)}
+      />
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+      {errorMsg && <FormErrorMessage>{errorMsg}</FormErrorMessage>}
+    </FormControl>
+  );
 };
 
 export default TextInput;
