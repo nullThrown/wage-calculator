3d1b86fd63326a290839b9076b428f6e85ecb642
Weston Peters <westonrpr@yahoo.com>
2023-06-19 19:52:45 -0500

    [fix] syntax error

--- client/src/features/auth/hooks/useLoginUser.js
+++ client/src/features/auth/hooks/useLoginUser.js
@@ -1,8 +1,6 @@
 import { useMutation } from 'react-query';
 import { loginUser } from '../api/auth';
 
-const useLoginUser = useMutation((user) => loginUser(user));
-// useRegisterUSer instance should handle the UI logic
-// error/success states, redirects etc.
+const useLoginUser = () => useMutation((user) => loginUser(user));
 
 export default useLoginUser;
