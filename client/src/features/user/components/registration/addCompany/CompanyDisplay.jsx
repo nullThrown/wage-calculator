36f2259f79ba3c3fa68cf5411ce4f378c08c0383
Weston Peters <westonrpr@yahoo.com>
2023-06-19 19:54:34 -0500

    [replace] in-line toast objects with import

--- client/src/features/user/components/registration/addCompany/CompanyDisplay.jsx
+++ client/src/features/user/components/registration/addCompany/CompanyDisplay.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
   Box,
   Table,
@@ -9,39 +8,27 @@ import {
   Td,
   TableContainer,
   Text,
-  Button,
   useToast,
 } from '@chakra-ui/react';
+import SetCompanyToEditBtn from 'components/button/SetCompanyToEditBtn';
+import DeleteCompanyBtn from 'components/button/DeleteCompanyBtn';
+import { successToast, errorToast } from 'components/toast/toast';
 import useDeleteCompany from 'features/user/hooks/usedeleteCompany';
 
 const CompanyDisplay = ({ companyList, setCompanyList, handleSetEditMode }) => {
   const deleteCompany = useDeleteCompany();
   const toast = useToast();
 
-  // const handleEditCompany = () => {};
   const handleDeleteCompany = (_id) => {
     deleteCompany.mutate(_id, {
       onSuccess: (data, variables, context) => {
         setCompanyList((prev) => {
           return prev.filter((company) => company._id !== _id);
         });
-        toast({
-          title: 'Company removed Successfully!',
-          status: 'success',
-          duration: 5000,
-          isClosable: true,
-          position: 'top',
-        });
+        toast({ ...successToast, title: 'Company removed Successfully!' });
       },
       onError: (error, variables, context) => {
-        toast({
-          title: 'Company removed Unsuccessfully.',
-          description: 'Please try again.',
-          status: 'error',
-          duration: 5000,
-          isClosable: true,
-          position: 'top',
-        });
+        toast({ ...errorToast, title: 'Company removed Unsuccessfully' });
       },
     });
   };
@@ -75,27 +62,18 @@ const CompanyDisplay = ({ companyList, setCompanyList, handleSetEditMode }) => {
               return (
                 <Tr
                   key={_id}
-                  bg={index % 2 === 1 ? 'rgba(240,240,240,.3)' : ''}>
+                  bg={index % 2 === 1 ? 'rgba(240,240,240,.3)' : null}>
                   <Td>{name}</Td>
                   <Td>{position}</Td>
                   <Td>$ {hourlyWage}</Td>
                   <Td>
                     {overtimeMultiplier}
-                    <Button
-                      ml='2em'
-                      size='xs'
-                      variant='link'
-                      onClick={() => handleSetEditMode(company)}>
-                      Edit
-                    </Button>
-                    <Button
-                      ml='.8em'
-                      size='xs'
-                      variant='outline'
-                      colorScheme='red'
-                      onClick={() => handleDeleteCompany(_id)}>
-                      Del
-                    </Button>
+                    <SetCompanyToEditBtn
+                      handleSetEditMode={() => handleSetEditMode(company)}
+                    />
+                    <DeleteCompanyBtn
+                      handleDeleteCompany={() => handleDeleteCompany(_id)}
+                    />
                   </Td>
                 </Tr>
               );
