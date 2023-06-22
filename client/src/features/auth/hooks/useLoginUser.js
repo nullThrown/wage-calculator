4ff59b4751ff6fdcfb7f2b06e15345f61ff0818f
Weston Peters <westonrpr@yahoo.com>
2023-06-21 20:40:18 -0500

    [feat] add onSuccess & onError callbacks
    These methods will conduct operations in the future. For the time being, more research will need to be done as to where the best location for sucess/error handling should be done in react-query.

--- client/src/features/auth/hooks/useLoginUser.js
+++ client/src/features/auth/hooks/useLoginUser.js
@@ -1,6 +1,13 @@
 import { useMutation } from 'react-query';
 import { loginUser } from '../api/auth';
+import storage from 'util/storage';
 
-const useLoginUser = () => useMutation((user) => loginUser(user));
+const useLoginUser = () =>
+  useMutation((user) => loginUser(user), {
+    onSuccess: (data, error, context) => {
+      storage.setToken(data.token);
+    },
+    onError: (error, data, context) => {},
+  });
 
 export default useLoginUser;
