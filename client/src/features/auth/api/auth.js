390cc0707ab1aa018e7b9347ed7af70d266dbb78
Weston Peters <westonrpr@yahoo.com>
2023-06-21 19:57:01 -0500

    [fix] signup/login routes error return values
    Return values need to be rejected promises and contain message for particular error.

--- client/src/features/auth/api/auth.js
+++ client/src/features/auth/api/auth.js
@@ -1,24 +1,23 @@
 import axios from 'config/axios';
+import { connection_error } from 'constants/api/error';
 
-// error returns might require a promise reject
 export const registerUser = async (user) => {
   try {
     const res = await axios.post('/auth/register', user);
     return res;
   } catch (err) {
-    // console.log(err.response);
-    const errorMsg = !err.response ? 'connection_error' : err.response.data.msg;
+    const errorMsg = !err.response ? connection_error : err.response.data.msg;
     return Promise.reject(new Error(errorMsg));
   }
 };
 
 export const loginUser = async (user) => {
   try {
-    const res = await axios.post('/auth/login', user);
-    return res;
+    const { data } = await axios.post('/auth/login', user);
+    return data;
   } catch (err) {
-    console.log(err);
-    return err;
+    const errorMsg = !err.response ? connection_error : err.response.data.msg;
+    return Promise.reject(new Error(errorMsg));
   }
 };
 
