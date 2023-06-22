9e91a395f78d4921ca146c91bad2f3ea99a364b8
Weston Peters <westonrpr@yahoo.com>
2023-06-21 20:40:50 -0500

    initial commit

---
+++ client/src/constants/api/error.js
@@ -0,0 +1,7 @@
+export const token_invalid = 'token_invalid';
+export const resource_not_found = 'resource_not_found';
+export const invalid_data = 'invalid_data';
+export const invalid_credentials = 'invalid_credentials';
+export const email_already_exists = 'email_already_exists';
+export const server_error = 'server_error';
+export const connection_error = 'connection_error';
