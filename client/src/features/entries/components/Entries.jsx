6100b390337f4a0124d80b27e5743f3b46123347
Weston Peters <westonrpr@yahoo.com>
2023-06-26 16:27:33 -0500

    create date initial state var & [fix] arg list for useGetEntriesByWeek hook

--- client/src/features/entries/components/Entries.jsx
+++ client/src/features/entries/components/Entries.jsx
@@ -1,20 +1,20 @@
 import Week from 'features/entries/components/Week';
 import Day from 'features/entries/components/Day';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import useGetEntriesByWeek from '../hooks/useGetWeeklyEntries';
 
-const Entries = ({ filter }) => {
-  const [date, setDate] = useState(
-    new Date()
-      .toLocaleDateString('en-US', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-      })
-      .replace(/\//g, '-')
-  );
+const currentDate = new Date()
+  .toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  })
+  .replace(/\//g, '-');
 
-  const { isLoading, isError, data } = useGetEntriesByWeek(date, filter);
+const Entries = ({ filter }) => {
+  const [date, setDate] = useState(currentDate);
+
+  const { isLoading, isError, data } = useGetEntriesByWeek(filter, date);
   return (
     <>
       <Day
