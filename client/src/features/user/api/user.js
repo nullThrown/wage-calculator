066bb6774aee1150b9666103cfb1e6c5796884b6
Weston Peters <westonrpr@yahoo.com>
2023-06-26 16:52:13 -0500

    add getCompanies route & fix all requests to return only data property

--- client/src/features/user/api/user.js
+++ client/src/features/user/api/user.js
@@ -19,10 +19,20 @@ export const updateUser = async (user) => {
     return err;
   }
 };
+
+export const getCompanies = async () => {
+  try {
+    const { data } = await axios.get('/user/me');
+    return data.companies;
+  } catch (err) {
+    const errorMsg = !err.response ? connection_error : err.response.data.msg;
+    return Promise.reject(new Error(errorMsg));
+  }
+};
 export const addCompany = async (company) => {
   try {
-    const res = await axios.post('/user/company/create', company);
-    return res;
+    const { data } = await axios.post('/user/company/create', company);
+    return data;
   } catch (err) {
     const errorMsg = !err.response ? connection_error : err.response.data.msg;
     return Promise.reject(new Error(errorMsg));
@@ -30,8 +40,8 @@ export const addCompany = async (company) => {
 };
 export const updateCompany = async (company) => {
   try {
-    const res = await axios.put('/user/company/update', company);
-    return res;
+    const { data } = await axios.put('/user/company/update', company);
+    return data;
   } catch (err) {
     const errorMsg = !err.response ? 'connection_error' : err.response.data.msg;
     return Promise.reject(new Error(errorMsg));
@@ -39,8 +49,8 @@ export const updateCompany = async (company) => {
 };
 export const deleteCompany = async (_id) => {
   try {
-    const res = await axios.delete(`/user/company/delete/${_id}`);
-    return res;
+    const { data } = await axios.delete(`/user/company/delete/${_id}`);
+    return data;
   } catch (err) {
     const errorMsg = !err.response ? 'connection_error' : err.response.data.msg;
     return Promise.reject(new Error(errorMsg));
