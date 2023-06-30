a57b0ccc079300128cc7d1de57cd19378f36927a
Weston Peters <westonrpr@yahoo.com>
2023-06-30 18:57:14 -0500

    [remove] unnecessary properties

--- constants/mongooseTypes.js
+++ constants/mongooseTypes.js
@@ -1,6 +1,6 @@
 const requiredStr = { type: String, required: true, trim: true };
-const requiredNum = { type: Number, required: true, trim: true };
-const requiredBool = { type: Boolean, required: true, trim: true };
+const requiredNum = { type: Number, required: true };
+const requiredBool = { type: Boolean, required: true };
 const currentTime = { type: Date, default: Date.now };
 const requiredDate = { type: Date, required: true };
 
