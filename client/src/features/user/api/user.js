04fc65c804f6cad8dfb74305c6401c62d819d4f4
Weston Peters <westonrpr@yahoo.com>
2023-07-08 20:20:19 -0500

    [move] all company-related fetch queries to path: 'features/company/api/'

--- client/src/features/user/api/user.js
+++ client/src/features/user/api/user.js
@@ -3,8 +3,8 @@ import { connection_error } from 'constants/api/error';
 
 export const getUser = async () => {
   try {
-    const res = await axios.get('/user/me');
-    return res;
+    const { data } = await axios.get('/user/me');
+    return data;
   } catch (err) {
     console.log(err);
     return err;
@@ -17,51 +17,5 @@ export const updateUser = async (user) => {
   } catch (err) {
     console.log(err);
     return err;
-  }
-};
-
-export const getCompanies = async () => {
-  try {
-    const { data } = await axios.get('/user/me');
-    return data.companies;
-  } catch (err) {
-    const errorMsg = !err.response ? connection_error : err.response.data.msg;
-    return Promise.reject(new Error(errorMsg));
-  }
-};
-export const addCompany = async (company) => {
-  try {
-    const { data } = await axios.post('/user/company/create', company);
-    return data;
-  } catch (err) {
-    const errorMsg = !err.response ? connection_error : err.response.data.msg;
-    return Promise.reject(new Error(errorMsg));
-  }
-};
-export const updateCompany = async (company) => {
-  try {
-    const { data } = await axios.put('/user/company/update', company);
-    return data;
-  } catch (err) {
-    const errorMsg = !err.response ? 'connection_error' : err.response.data.msg;
-    return Promise.reject(new Error(errorMsg));
-  }
-};
-export const deleteCompany = async (_id) => {
-  try {
-    const { data } = await axios.delete(`/user/company/delete/${_id}`);
-    return data;
-  } catch (err) {
-    const errorMsg = !err.response ? 'connection_error' : err.response.data.msg;
-    return Promise.reject(new Error(errorMsg));
-  }
-};
-export const setCompanyRemovedStatus = async (body) => {
-  try {
-    const res = await axios.put('/user/company/remove/set', body);
-    return res;
-  } catch (err) {
-    const errorMsg = !err.response ? 'connection_error' : err.response.data.msg;
-    return Promise.reject(new Error(errorMsg));
   }
 };
