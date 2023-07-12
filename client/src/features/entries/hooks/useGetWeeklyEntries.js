67884a8d215cd4539ef2bd15e1fc667270610192
Weston Peters <westonrpr@yahoo.com>
2023-07-12 18:59:53 -0500

    [fix] hook return values

--- client/src/features/entries/hooks/useGetWeeklyEntries.js
+++ client/src/features/entries/hooks/useGetWeeklyEntries.js
@@ -2,9 +2,10 @@ import { useQuery } from 'react-query';
 import { getEntriesByWeek } from '../api/entries';
 
 const useGetEntriesByWeek = (filter, date) => {
-  return useQuery(['entries', filter, date], () =>
+  const { isLoading, isError, data } = useQuery(['entries', filter, date], () =>
     getEntriesByWeek(filter, date)
   );
+  return { isLoading, isError, data };
 };
 
 export default useGetEntriesByWeek;
