190c59ebe5f676e8ae0f3dffae5d619332767225
Weston Peters <westonrpr@yahoo.com>
2023-07-12 18:59:27 -0500

    [refactor] week data derivations from useState to useReducer

--- client/src/features/entries/components/week/Week.jsx
+++ client/src/features/entries/components/week/Week.jsx
@@ -1,69 +1,39 @@
-import { useState, useEffect, useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import LargeCard from 'components/card/LargeCard';
 import TertHeading from 'components/typography/TertHeading';
 import DateBox from './DateBox';
 import WeekSelect from './WeekSelect';
 import { HStack, Spinner } from '@chakra-ui/react';
-import ErrorText from 'components/typography/ErrorText';
 import SomethingWentWrong from 'components/typography/SomethingWentWrong';
-// this whole state could be simplified with a useReducer fn
-
-const weekDataReducer = (state, action) => {
-  switch (action.type) {
-    case 'init_data':
-      break;
-
-    case 'switch_week':
-      break;
-    default:
-      throw new error('no action');
-      break;
-  }
-};
+import useGetEntriesByWeek from 'features/entries/hooks/useGetWeeklyEntries';
+import formatReadableDate from 'util/formatReadableDate';
+import weekDataReducer from 'features/entries/helpers/weekDataReducer';
 
 const initialData = {
   weekDates: [],
+  currentWeek: '',
   selectedWeekData: {},
-  selectedDays: [],
 };
 
-const Week = ({ filter, entries, isLoading, isError }) => {
-  const [weekPairs, setWeekPairs] = useState([]);
-  const [selectedWeek, setSelectedWeek] = useState('');
-  const [selectedWeekData, setSelectedWeekData] = useState({});
-  const [selectedDays, setSelectedDays] = useState([]);
+const Week = ({ filter }) => {
+  const { isLoading, isError, data } = useGetEntriesByWeek(filter, new Date());
 
-  const [state, dispatch] = useReducer(weekDataReducer, initialData);
+  const [{ weekDates, currentWeek, selectedWeekData }, dispatch] = useReducer(
+    weekDataReducer,
+    initialData
+  );
 
-  const selectChangeHandler = (e) => setSelectedWeek(e.target.value);
-
-  useEffect(() => {
-    setWeekPairs(() => {
-      return entries?.map((week) => {
-        return week.datesShort;
-      });
+  const selectChangeHandler = (e) =>
+    dispatch({
+      type: 'switch_week',
+      payload: { selectedWeek: e.target.value, data },
     });
-  }, [entries]);
 
   useEffect(() => {
-    setSelectedWeek(entries?.[0].datesShort);
-  }, [entries]);
-
-  useEffect(() => {
-    setSelectedWeekData(() =>
-      entries?.find((week) => week.datesShort === selectedWeek)
-    );
-  }, [selectedWeek]);
-
-  useEffect(() => {
-    setSelectedDays(() =>
-      selectedWeekData?.weekOfDays?.map((day) => {
-        const dayDate = new Date(day);
-        dayDate.setUTCHours(5);
-        return dayDate.getDate();
-      })
-    );
-  }, [selectedWeekData]);
+    if (data) {
+      dispatch({ type: 'init_data', payload: data });
+    }
+  }, [data]);
 
   if (isLoading) {
     return <Spinner />;
@@ -78,17 +48,18 @@ const Week = ({ filter, entries, isLoading, isError }) => {
       <TertHeading textAlign='center'>Week</TertHeading>
       <WeekSelect
         selectChangeHandler={selectChangeHandler}
-        selectedWeek={selectedWeek}
-        weekPairs={weekPairs}
+        selectedWeek={currentWeek}
+        weekPairs={weekDates}
       />
       <HStack>
-        {selectedDays?.map((day, i) => {
-          const entriesByDay = selectedWeekData?.entries.filter((entry) => {
-            const shiftDate = new Date(entry.shiftDate);
-            return shiftDate.getDate() === day;
+        {selectedWeekData.weekOfDays?.map((day, i) => {
+          const dayDate = formatReadableDate(day);
+          const entriesByDay = selectedWeekData.entries.filter((entry) => {
+            const shiftDate = formatReadableDate(entry.shiftDate);
+            return shiftDate === dayDate;
           });
 
-          return <DateBox key={i} day={day} entries={entriesByDay} />;
+          return <DateBox key={i} day={dayDate} entries={entriesByDay} />;
         })}
       </HStack>
     </LargeCard>
