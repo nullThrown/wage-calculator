cf0a248ce25242a706b79aa979df5c4bd9894d9c
Weston Peters <westonrpr@yahoo.com>
2023-07-20 21:44:47 -0500

    [feat] combine both Day and Week components into this component

--- client/src/features/entries/components/Entries.jsx
+++ client/src/features/entries/components/Entries.jsx
@@ -1,24 +1,67 @@
-import Week from 'features/entries/components/week/Week';
-import Day from 'features/entries/components/day/Day';
-import { useState } from 'react';
+import LargeCard from 'components/card/LargeCard';
+import TertHeading from 'components/typography/TertHeading';
+import { Spinner, HStack } from '@chakra-ui/react';
+import DatePicker from 'react-datepicker';
+import { useEffect, useState } from 'react';
+import SomethingWentWrong from 'components/typography/SomethingWentWrong';
+import DataDisplay from 'features/entries/components/day/DataDisplay';
+import DataDisplayEmpty from 'features/entries/components/day/DataDisplayEmpty';
+import useGetEntriesByWeek from 'features/entries/hooks/useGetWeeklyEntries';
+import formatReadableDate from 'util/formatReadableDate';
+import DateBox from 'features/entries/components/week/DateBox';
+import selectWeekData from 'features/entries/helpers/selectWeekData';
 
-const currentDate = new Date()
-  .toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  })
-  .replace(/\//g, '-');
+const currentDate = new Date();
 
 const Entries = ({ filter }) => {
-  const [date, setDate] = useState(currentDate);
+  const [date, setDate] = useState(new Date());
+  const [selectedEntry, setSelectedEntry] = useState(null);
 
+  const { isLoading, isError, data } = useGetEntriesByWeek(filter, currentDate);
+
+  const selectedWeekData = selectWeekData(date, data);
+  useEffect(() => {
+    console.log(selectedEntry);
+  }, [selectedEntry]);
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (isError) {
+    return <SomethingWentWrong />;
+  }
   return (
-    <>
-      <Day filter={filter} />
-      <Week filter={filter} date={date} />
-    </>
+    <LargeCard as='section'>
+      <TertHeading textAlign='center'>Entries</TertHeading>
+      <DatePicker
+        selected={date}
+        onChange={(date) => setDate(date)}
+        maxDate={new Date()}></DatePicker>
+
+      <HStack>
+        {selectedWeekData.weekOfDays?.map((day, i) => {
+          const dayDate = formatReadableDate(day);
+          const entriesByDay = selectedWeekData.entries.filter((entry) => {
+            const shiftDate = formatReadableDate(entry.shiftDate);
+            return shiftDate === dayDate;
+          });
+
+          return (
+            <DateBox
+              key={day}
+              day={dayDate}
+              entries={entriesByDay}
+              setSelectedEntry={setSelectedEntry}
+            />
+          );
+        })}
+      </HStack>
+
+      {selectedEntry ? (
+        <DataDisplay selectedEntry={selectedEntry} />
+      ) : (
+        <DataDisplayEmpty />
+      )}
+    </LargeCard>
   );
 };
-
 export default Entries;
