18d69c73486af8950314e1f55ebef662ac9fe073
Weston Peters <westonrpr@yahoo.com>
2023-07-28 21:48:04 -0500

    initial commit

---
+++ client/src/components/inputs/DatePicker/CustomDatePicker.jsx
@@ -0,0 +1,25 @@
+import { forwardRef } from 'react';
+import DatePicker from 'react-datepicker';
+import './CustomDatePicker.css';
+const CustomDatePicker = ({ date, setDate }) => {
+  const ExampleCustomInput = forwardRef(({ value, onClick, onChange }, ref) => (
+    <input
+      value={value}
+      className='example-custom-input'
+      onClick={onClick}
+      onChange={onChange}
+      ref={ref}></input>
+  ));
+  return (
+    <DatePicker
+      selected={date}
+      onChange={(date) => setDate(date)}
+      customInput={<ExampleCustomInput />}
+      dayClassName={() => 'example-datepicker-day-class'}
+      popperClassName='example-datepicker-class'
+      todayButton='TODAY'
+    />
+  );
+};
+
+export default CustomDatePicker;
