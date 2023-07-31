059db0068a91bb1312b55f614888ca0e145f3e45
Weston Peters <westonrpr@yahoo.com>
2023-07-31 13:42:27 -0500

    [feat] add test route to both protected & public routes

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -4,15 +4,16 @@ import NotFound from 'pages/public/NotFound';
 import protectedRoutes from './protected';
 import publicRoutes from './public';
 import useAuth from 'features/auth/hooks/useAuth';
+import Test from 'pages/public/Test';
 // const protectedRoutes = lazy(() => import('./protected'));
 // const publicRoutes = lazy(() => import('./public'));
 
 const AppRoutes = () => {
   const errorRoute = { path: '/*', element: <NotFound /> };
-
+  const testRoute = { path: '/test', element: <Test /> };
   const isAuth = useAuth();
   const routes = isAuth ? protectedRoutes : publicRoutes;
-  const element = useRoutes([...routes, errorRoute]);
+  const element = useRoutes([...routes, testRoute, errorRoute]);
 
   return <>{element}</>;
 };
