1d29e9755752377454eb396e27d2e3e6ab344522
Weston Peters <westonrpr@yahoo.com>
2023-08-02 18:50:23 -0500

    add header styles

--- client/src/components/base/Header.jsx
+++ client/src/components/base/Header.jsx
@@ -1,4 +1,7 @@
-import { Box } from '@chakra-ui/react';
+import { Box, HStack } from '@chakra-ui/react';
+import QuatHeading from 'components/typography/QuatHeading';
+import Login from 'components/link/Login';
+import Signup from 'components/link/Signup';
 const Header = () => {
   return (
     <Box
@@ -6,8 +9,18 @@ const Header = () => {
       maxW='1000px'
       w='98%'
       m='0 auto'
-      h='80px'
-      borderBottom='1px solid rgba(44, 105, 185, .8)'></Box>
+      h='3em'
+      display='flex'
+      alignItems='flex-end'
+      paddingBottom='.4em'
+      justifyContent='space-between'
+      borderBottom='1px solid rgba(44, 105, 185, .8)'>
+      <QuatHeading as='h1'>Tip Analytics</QuatHeading>
+      <HStack>
+        <Login />
+        <Signup />
+      </HStack>
+    </Box>
   );
 };
 
