6ab34aea91760fefdd697cc374b5e8c02348a857
Weston Peters <westonrpr@yahoo.com>
2023-08-02 18:50:55 -0500

    [remove] margin

--- client/src/components/base/TestContainer.jsx
+++ client/src/components/base/TestContainer.jsx
@@ -2,7 +2,7 @@ import { Container } from '@chakra-ui/react';
 
 const TestContainer = ({ children }) => {
   return (
-    <Container as='main' maxW='1000px' w='98%' centerContent m='8em auto 0'>
+    <Container as='main' maxW='1000px' w='98%' centerContent>
       {children}
     </Container>
   );
