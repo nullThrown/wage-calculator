26473defd312b1542cf39ed8dd8cff07728479fc
Weston Peters <westonrpr@yahoo.com>
2023-08-02 18:52:29 -0500

    add styles

--- client/src/components/link/Signup.jsx
+++ client/src/components/link/Signup.jsx
@@ -2,7 +2,20 @@ import { Link } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 const Signup = () => {
   return (
-    <Link as={RouterLink} to='/signup'>
+    <Link
+      as={RouterLink}
+      to='/signup'
+      color='blackAlpha.800'
+      // blue.600
+      border='1px solid rgba(0, 0, 0, 0.80)'
+      padding='2px 12px'
+      borderRadius='14px'
+      backgroundColor='white'
+      _hover={{
+        textDecoration: 'none',
+        color: 'blackAlpha.600',
+        borderColor: 'blackAlpha.600',
+      }}>
       Signup
     </Link>
   );
