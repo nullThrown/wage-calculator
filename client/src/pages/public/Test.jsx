1f383eb01906c83aede6b62730d94095ecb1ddc2
Weston Peters <westonrpr@yahoo.com>
2023-08-07 16:15:14 -0500

    unofficial test of the MainWrapper component

--- client/src/pages/public/Test.jsx
+++ client/src/pages/public/Test.jsx
@@ -1,10 +1,16 @@
 import TestContainer from 'components/base/TestContainer';
 import Header from 'components/base/Header';
+import MainWrapper from 'components/base/MainWrapper';
+import { Box } from '@chakra-ui/react';
+import Footer from 'components/base/Footer';
+import Landing from './Landing';
 const Test = () => {
   return (
-    <TestContainer>
+    <MainWrapper isHeader>
       <Header />
-    </TestContainer>
+      <Box as='main'>this is some main content</Box>
+      <Footer />
+    </MainWrapper>
   );
 };
 
