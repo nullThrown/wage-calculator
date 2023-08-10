59512a7e16afd76c1bf2290ff36a788b57d61bba
Weston Peters <westonrpr@yahoo.com>
2023-08-09 21:14:01 -0500

    [feat] destructure return object

--- client/src/features/user/hooks/useGetUser.js
+++ client/src/features/user/hooks/useGetUser.js
@@ -1,6 +1,10 @@
 import { useQuery } from 'react-query';
 import { getUser } from 'features/user/api/user';
 
-const useGetUser = () => useQuery(['user'], () => getUser());
+const useGetUser = () => {
+  const { data: user, isLoading, isError } = useQuery(['user'], getUser);
+
+  return { isLoading, isError, user };
+};
 
 export default useGetUser;
