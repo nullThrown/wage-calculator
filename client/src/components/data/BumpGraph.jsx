2850167d0d4f52f28c389a2680fe4dd087fdc789
Weston Peters <westonrpr@yahoo.com>
2023-08-14 12:30:18 -0500

    initial commit

---
+++ client/src/components/data/BumpGraph.jsx
@@ -0,0 +1,11 @@
+import { ResponsiveBump } from '@nivo/bump';
+import { Box } from '@chakra-ui/react';
+const BumpGraph = () => {
+  return (
+    <Box width='22rem' height='18rem'>
+      <ResponsiveBump />;
+    </Box>
+  );
+};
+
+export default BumpGraph;
