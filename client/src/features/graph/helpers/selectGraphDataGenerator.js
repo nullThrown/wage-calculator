818408ae5b270454fe84c7506b73fa0517d7d7a8
Weston Peters <westonrpr@yahoo.com>
2023-08-18 19:50:42 -0500

    remove comments & by year filter condition

--- client/src/features/graph/helpers/selectGraphDataGenerator.js
+++ client/src/features/graph/helpers/selectGraphDataGenerator.js
@@ -4,36 +4,17 @@ import byMonthPerHour from './byMonthPerHour';
 import byMonthPerShift from './byMonthPerShift';
 
 const selectGraphDataGenerator = (entries, filters) => {
-  // if !entries return default/dummy data
   if (!entries)
     return () => [{ id: 'no-data', data: [{ x: 'no-data', y: 40 }] }];
 
-  const { XAxis, shiftTime, YAxis } = filters;
-  // if X == 'byDay'
+  const { XAxis, YAxis } = filters;
   if (XAxis === 'byDay') {
-    // created nest condition
-    // if(y == 'totalPerHour' ) return byDayPerHour.js
     if (YAxis === 'totalPerHour') return byDayPerHour;
-    // if(y == 'avgTotalPerShift' ) return byDayPerShift.js
     if (YAxis === 'avgTotalPerShift') return byDayPerShift;
   }
-  // step out of nested condition
-  // if x == 'byMonth'
   if (XAxis === 'byMonth') {
-    // if(y == 'totalPerHour' ) return byMonthPerHour.js
     if (YAxis === 'totalPerHour') return byMonthPerHour;
-    // if(y == 'avgTotalPerShift' ) return byMonthPerShift.js
     if (YAxis === 'avgTotalPerShift') return byMonthPerShift;
   }
-  // // step out of nested condition
-  // // if x == 'byYear'
-  // if (XAxis === 'byYear') {
-  //   // if(y == 'totalPerHour' ) return byYearPerHour.js
-  //   if (YAxis === 'totalPerHour') return byYearPerHour;
-  //   // if(y == 'avgTotalPerShift' ) return byYearPerShift.js
-  //   if (YAxis === 'avgTotalPerShift') return byYearPerShift;
-  // }
-
-  // return value of any function
 };
 export default selectGraphDataGenerator;
