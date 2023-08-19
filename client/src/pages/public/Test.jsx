4cc6ed540c65b995c312ddc492bdaa998adfda98
Weston Peters <westonrpr@yahoo.com>
2023-08-18 19:07:51 -0500

    tested line graph

--- client/src/pages/public/Test.jsx
+++ client/src/pages/public/Test.jsx
@@ -1,11 +1,15 @@
 import TestContainer from 'components/base/TestContainer';
 import { Box, Text } from '@chakra-ui/react';
-
+import LineGraph from 'components/data/LineGraph';
 const Test = () => {
   return (
     <TestContainer>
       <Text>Hello there</Text>
-      <Box height='24rem' width='36rem'></Box>
+      <Box height='24rem' width='36rem'>
+        <LineGraph
+          data={[{ id: 'no-data', data: [{ x: 'no-data', y: 40 }] }]}
+        />
+      </Box>
     </TestContainer>
   );
 };
