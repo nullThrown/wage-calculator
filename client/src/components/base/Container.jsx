59eb31c56bbf428b837f672d8ed507383262ae6e
Weston Peters <westonrpr@yahoo.com>
2023-08-21 23:06:16 -0500

    alter styles

--- client/src/components/base/Container.jsx
+++ client/src/components/base/Container.jsx
@@ -2,7 +2,7 @@ import { Container } from '@chakra-ui/react';
 
 const MainContainer = ({ children }) => {
   return (
-    <Container as='main' centerContent maxW='80rem' w='98%' m='8rem auto 4rem'>
+    <Container as='main' centerContent maxW='80rem' w='98%' m='0 auto 4rem'>
       {children}
     </Container>
   );
