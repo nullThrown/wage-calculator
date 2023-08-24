62f64753aa14d7c91913352da82372e9065533ce
Weston Peters <westonrpr@yahoo.com>
2023-08-23 22:05:53 -0500

    [feat] add dynamic grid row config

--- client/src/components/base/MainWrapper.jsx
+++ client/src/components/base/MainWrapper.jsx
@@ -1,11 +1,16 @@
 import { Box } from '@chakra-ui/react';
-const MainWrapper = ({ children, isHeader }) => {
+const MainWrapper = ({ children, numOfRows }) => {
+  const gridRowConfig = {
+    two: '1fr auto',
+    three: 'auto 1fr auto',
+    four: 'auto auto 1fr auto',
+  };
   return (
     <Box
       minH='100vh'
       display='grid'
-      gridTemplateRows={isHeader ? 'auto 1fr auto' : '1fr auto'}
-      gridTemplateColumns='100%'>
+      gridTemplateColumns='100%'
+      gridTemplateRows={gridRowConfig[numOfRows]}>
       {children}
     </Box>
   );
