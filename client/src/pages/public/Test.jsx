eb9232fcbd6d0884a5a176c77a925fc8f231b529
Weston Peters <westonrpr@yahoo.com>
2023-08-25 21:11:47 -0500

    test signup success page

--- client/src/pages/public/Test.jsx
+++ client/src/pages/public/Test.jsx
@@ -1,6 +1,11 @@
 import TwoColumnLayout from 'components/layout/TwoColumnLayout';
+import SignupSuccess from 'pages/protected/SignupSuccess';
 const Test = () => {
-  return <TwoColumnLayout></TwoColumnLayout>;
+  return (
+    <TwoColumnLayout>
+      <SignupSuccess />
+    </TwoColumnLayout>
+  );
 };
 
 export default Test;
