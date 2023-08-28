d05640dff9ea45d4cb8f35a1816b4bd52b434fee
Weston Peters <westonrpr@yahoo.com>
2023-08-28 18:17:02 -0500

    clean file

--- client/src/features/auth/hooks/useRegisterUser.js
+++ client/src/features/auth/hooks/useRegisterUser.js
@@ -2,17 +2,11 @@ import { useMutation } from 'react-query';
 import { registerUser } from '../api/auth';
 import storage from '../../../services/storage';
 
-// const useRegisterUser = useMutation((user) => registerUser(user));
-// useRegisterUSer instance should handle the UI logic
-// error/success states, redirects etc.
-
 function useRegisterUser(user) {
   return useMutation(() => registerUser(user), {
-    onError: (error, variables, context) => {
-      // console.log(error);
-    },
+    onError: (error, variables, context) => {},
     onSuccess: (data, variables, context) => {
-      storage.setToken(data.data.token);
+      storage.setToken(data.token);
     },
   });
 }
