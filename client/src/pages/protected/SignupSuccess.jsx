e21c547dad358c36d7d382dbeb763c74ae814abe
Weston Peters <westonrpr@yahoo.com>
2023-08-28 18:18:06 -0500

    [fix] component animation timing & styles

--- client/src/pages/protected/SignupSuccess.jsx
+++ client/src/pages/protected/SignupSuccess.jsx
@@ -1,7 +1,7 @@
 import { Box, VStack, keyframes, Text } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { useEffect } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const widgetKeyframes = keyframes`
   0% { transform: scale(1) rotate(0); border-radius: 20%; }
@@ -10,40 +10,50 @@ const widgetKeyframes = keyframes`
   75% { transform: scale(1) rotate(270deg); border-radius: 50%; }
   100% { transform: scale(1) rotate(0); border-radius: 20%; }
 `;
-
-const widgetAnimation = `${widgetKeyframes} 1800ms ease-in-out 0s 1.5 normal forwards`;
 const textKeyFrames = keyframes`
 100% { transform: translateY(-15px); opacity:1  }`;
 
-const textAnimation = `${textKeyFrames} 500ms ease-out 2700ms normal forwards`;
+const textAnimation = `${textKeyFrames} 700ms ease-out  normal forwards`;
 
-const SignupSuccess = () => {
+const SignupSuccess = ({ isSuccess }) => {
+  let iterationCount;
+  if (isSuccess === true) iterationCount = '.5';
+  else iterationCount = 'infinite';
+
+  console.log(iterationCount);
+
+  const widgetAnimation = `${widgetKeyframes} 1800ms ease-in-out 0s ${iterationCount} normal forwards`;
   const navigate = useNavigate();
+
   useEffect(() => {
-    setTimeout(() => {
-      navigate('/add-company');
-    }, 3500);
-  }, []);
+    if (isSuccess) {
+      setTimeout(() => {
+        navigate('/add-company');
+      }, 1500);
+    }
+  }, [isSuccess]);
+
   return (
     <VStack spacing='2' mt='10em'>
       <Box
         as={motion.div}
         animation={widgetAnimation}
         padding='2'
-        // bgGradient='linear(to-l, #7928CA, #FF0080)'
         backgroundColor='green.400'
         width='12'
         height='12'
         display='flex'></Box>
-      <Text
-        mt='2em'
-        fontSize='2xl'
-        color='green.400'
-        opacity='0'
-        as={motion.p}
-        animation={textAnimation}>
-        Signup Successful!
-      </Text>
+      {isSuccess && (
+        <Text
+          mt='2em'
+          fontSize='2xl'
+          color='green.400'
+          opacity='0'
+          as={motion.p}
+          animation={textAnimation}>
+          Account Created!
+        </Text>
+      )}
     </VStack>
   );
 };
