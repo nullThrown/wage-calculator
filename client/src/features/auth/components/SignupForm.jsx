610876bf0b3efb4841c6ca1834b51b468829cda4
Weston Peters <westonrpr@yahoo.com>
2023-08-28 18:16:34 -0500

    [feat] add loading state and fix validation

--- client/src/features/auth/components/SignupForm.jsx
+++ client/src/features/auth/components/SignupForm.jsx
@@ -5,12 +5,13 @@ import useRegisterUser from 'features/auth/hooks/useRegisterUser';
 import useSignupValidation from 'features/auth/hooks/useValidateSignup';
 import TextInput from 'components/form/TextInput';
 import SignupBtn from 'components/button/SignupBtn';
-// import SignupSuccess from 'pages/protected/SignupSuccess';
+import SignupSuccess from 'pages/protected/SignupSuccess';
 
 import {
   connection_error,
   email_already_exists,
   server_error,
+  invalid_data,
 } from 'constants/api/error';
 import { errorToast } from 'components/toast/toast';
 
@@ -22,11 +23,11 @@ const SignupForm = () => {
   });
   const [displayErrors, setDisplayErrors] = useState(false);
 
-  const navigate = useNavigate();
   const toast = useToast();
   const { emailError, usernameError, passwordError, setEmailError } =
     useSignupValidation(user);
-  const registerUser = useRegisterUser(user);
+  const { isLoading, isIdle, isSuccess, isError, mutate } =
+    useRegisterUser(user);
 
   const handleInputChange = (e) => {
     setUser(() => {
@@ -40,14 +41,13 @@ const SignupForm = () => {
       return setDisplayErrors(true);
     }
 
-    return registerUser.mutate(user, {
-      onSuccess: (data, variables, context) => {
-        navigate('/signup-success');
-      },
+    return mutate(user, {
       onError: (error, variables, context) => {
         const { message } = error;
-        if (message === email_already_exists) {
+        console.log(message);
+        if (message === email_already_exists || invalid_data) {
           setEmailError(true);
+          setDisplayErrors(true);
         } else if (message === server_error || message === connection_error) {
           toast({ ...errorToast });
         }
@@ -55,74 +55,76 @@ const SignupForm = () => {
     });
   };
 
-  return (
-    <>
-      <Heading
-        as='h1'
-        size='xl'
-        textAlign='center'
-        fontWeight='400'
-        color='#20499C'>
-        Sign Up
-      </Heading>
-      <Flex
-        m={['1.4em auto 0', '1.8em auto 0', '1.8em 0 0']}
-        w={['92%', '74%', '100%']}
-        flexDirection={['column', 'column', 'row']}
-        alignItems='center'
-        gap={['3.2em', '3.2em', '.8em']}>
-        <Box
-          as='form'
-          width='100%'
-          order={[1, 1, 0]}
-          borderBottom='2px solid #20499C'>
-          <Flex flexDirection='column' gap='.8em'>
-            <TextInput
-              title='Email'
-              name='email'
-              type='email'
-              value={user.email}
-              onChange={handleInputChange}
-              isInvalid={emailError & displayErrors}
-              errorMsg='Email is either invalid or in use'
-            />
-            <TextInput
-              title='Username'
-              name='username'
-              value={user.username}
-              onChange={handleInputChange}
-              isInvalid={usernameError & displayErrors}
-              errorMsg='Must have a username'
-            />
+  if (isLoading || isSuccess) {
+    return <SignupSuccess isSuccess={isSuccess} />;
+  }
+  if (isIdle || isError) {
+    return (
+      <>
+        <Heading
+          as='h1'
+          size='xl'
+          textAlign='center'
+          fontWeight='400'
+          color='#20499C'>
+          Sign Up
+        </Heading>
+        <Flex
+          m={['1.4em auto 0', '1.8em auto 0', '1.8em 0 0']}
+          w={['92%', '74%', '100%']}
+          flexDirection={['column', 'column', 'row']}
+          alignItems='center'
+          gap={['3.2em', '3.2em', '.8em']}>
+          <Box
+            as='form'
+            width='100%'
+            order={[1, 1, 0]}
+            borderBottom='2px solid #20499C'>
+            <Flex flexDirection='column' gap='.8em'>
+              <TextInput
+                title='Email'
+                name='email'
+                type='email'
+                value={user.email}
+                onChange={handleInputChange}
+                isInvalid={emailError & displayErrors}
+                errorMsg='Email is either invalid or in use'
+              />
+              <TextInput
+                title='Username'
+                name='username'
+                value={user.username}
+                onChange={handleInputChange}
+                isInvalid={usernameError & displayErrors}
+                errorMsg='Must have a username'
+              />
 
-            <TextInput
-              title='password'
-              name='password'
-              type='password'
-              value={user.password}
-              onChange={handleInputChange}
-              isInvalid={passwordError & displayErrors}
-              errorMsg='Password must be at least 8 characters'
-              helperText={
-                passwordError & displayErrors
-                  ? null
-                  : 'Password must be at least 8 characters'
-              }
-            />
-          </Flex>
-          <Flex justify='center' m='2em 0 1em'>
-            <SignupBtn
-              isLoading={registerUser.isLoading}
-              handleSubmit={handleSubmit}
-            />
-          </Flex>
-        </Box>
-        <Box as='figure' w={['90%', '90%', '100%']}>
-          <Image src='svg/welcome-cats.svg' />
-        </Box>
-      </Flex>
-    </>
-  );
+              <TextInput
+                title='password'
+                name='password'
+                type='password'
+                value={user.password}
+                onChange={handleInputChange}
+                isInvalid={passwordError & displayErrors}
+                errorMsg='Password must be at least 8 characters'
+                helperText={
+                  passwordError & displayErrors
+                    ? null
+                    : 'Password must be at least 8 characters'
+                }
+              />
+            </Flex>
+            <Flex justify='center' m='2em 0 1em'>
+              <SignupBtn isLoading={isLoading} handleSubmit={handleSubmit} />
+            </Flex>
+          </Box>
+          <Box as='figure' w={['90%', '90%', '100%']}>
+            <Image src='svg/welcome-cats.svg' />
+          </Box>
+        </Flex>
+      </>
+    );
+  }
 };
 
 export default SignupForm;
