90d697e353a8e5d2863365e59f58994de0c795e0
Weston Peters <westonrpr@yahoo.com>
2023-08-29 13:22:40 -0500

    [feat] add date year/month

--- client/src/features/entries/components/displayEntry/calendar/Header.jsx
+++ client/src/features/entries/components/displayEntry/calendar/Header.jsx
@@ -1,10 +1,14 @@
 import { Box, Text, HStack } from '@chakra-ui/react';
 import SecHeading from 'components/typography/SecHeading';
-const Header = ({ children }) => {
+const Header = ({ date, children }) => {
+  const monthName = date.toLocaleString('default', { month: 'short' });
+  const fullYear = date.getFullYear();
   return (
     <Box as='header' mb='2em'>
       <HStack spacing='12px'>
-        <SecHeading as='h3'>July 2023</SecHeading>
+        <SecHeading as='h3'>
+          {monthName} {fullYear}
+        </SecHeading>
         {children}
       </HStack>
       <Text opacity='.85' mt='.4em'>
