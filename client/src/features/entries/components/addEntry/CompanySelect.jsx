7e3948d9a7cf04d8f4589190a7a0bd7783a15508
Weston Peters <westonrpr@yahoo.com>
2023-08-29 15:56:01 -0500

    [remove] loading and error states

--- client/src/features/entries/components/addEntry/CompanySelect.jsx
+++ client/src/features/entries/components/addEntry/CompanySelect.jsx
@@ -1,30 +1,25 @@
 import { Select, Flex, FormLabel } from '@chakra-ui/react';
-import useGetCompanies from 'features/company/hooks/useGetCompanies';
-import { Spinner } from '@chakra-ui/react';
-import SomethingWentWrong from 'components/typography/SomethingWentWrong';
 
-export const CompanySelect = ({ onChange, companyId }) => {
-  const { isLoading, isError, companyList } = useGetCompanies();
-
-  if (isLoading) {
-    return <Spinner />;
+export const CompanySelect = ({ onChange, companyId, companyList }) => {
+  if (companyList) {
+    return (
+      <Flex flexDirection='column' mr='2em'>
+        <FormLabel opacity='.85'>Company</FormLabel>
+        <Select
+          mt='.4em'
+          onChange={onChange}
+          value={companyId}
+          name='companyId'>
+          {companyList.map((company) => {
+            return (
+              <option key={company._id} value={company._id}>
+                {company.name}
+              </option>
+            );
+          })}
+        </Select>
+      </Flex>
+    );
   }
-  if (isError) {
-    return <SomethingWentWrong />;
-  }
-  return (
-    <Flex flexDirection='column' mr='2em'>
-      <FormLabel opacity='.85'>Company</FormLabel>
-      <Select mt='.4em' onChange={onChange} value={companyId} name='company'>
-        {companyList.map((company) => {
-          return (
-            <option key={company._id} value={company._id}>
-              {company.name}
-            </option>
-          );
-        })}
-      </Select>
-    </Flex>
-  );
 };
 export default CompanySelect;
