dcf95d1d4766d13ebf3beb14e4053343af3ca7e2
Weston Peters <westonrpr@yahoo.com>
2023-09-01 19:04:53 -0500

    add new field & cleanup file

--- models/Entry.js
+++ models/Entry.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const {
-  requiredNum,
-  requiredStr,
-  requiredDate,
-} = require('../constants/mongooseTypes');
+const { requiredNum, requiredStr } = require('../constants/mongooseTypes');
 
 const EntrySchema = new Schema(
   {
@@ -25,41 +21,11 @@ const EntrySchema = new Schema(
     totalWages: requiredNum,
     totalEarned: requiredNum,
     trueTotalEarned: requiredNum,
+    totalEarnedPerHour: requiredNum,
     tipPct: { ...requiredNum, default: -1 },
     trueTipPct: { ...requiredNum, default: -1 },
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
-// EntrySchema.virtual('totalTips').get(function () {
-//   return this.creditTips + this.cashTips;
-// });
-// EntrySchema.virtual('trueTotalTips').get(function () {
-//   return this.creditTips + this.cashTips - this.tipOut;
-// });
-// EntrySchema.virtual('totalWages').get(function () {
-//   return this.timeWorkedDec * this.hourlyWage;
-// });
-// EntrySchema.virtual('totalEarned').get(function () {
-//   return this.creditTips + this.cashTips + this.timeWorkedDec * this.hourlyWage;
-// });
-// EntrySchema.virtual('trueTotalEarned').get(function () {
-//   return (
-//     this.creditTips +
-//     this.cashTips -
-//     this.tipOut +
-//     this.timeWorkedDec * this.hourlyWage
-//   );
-// });
-// EntrySchema.virtual('tipPct').get(function () {
-//   if (this.totalSalesApplicable) {
-//     return (this.creditTips + this.cashTips) / this.totalSales;
-//   } else return -1;
-// });
-// EntrySchema.virtual('trueTipPct').get(function () {
-//   if (this.totalSalesApplicable) {
-//     return (this.creditTips + this.cashTips - this.tipOut) / this.totalSales;
-//   } else {
-//     return -1;
-//   }
-// });
+
 module.exports = EntrySchema;
