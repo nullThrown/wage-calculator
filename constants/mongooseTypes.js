f270c85a6bf31a3c4d0aeceb829ce892b133b44a
Weston Peters <westonrpr@yahoo.com>
2023-09-05 20:58:11 -0500

    [feat] add new type for mongoIds

--- constants/mongooseTypes.js
+++ constants/mongooseTypes.js
@@ -1,8 +1,11 @@
+const mongoose = require('mongoose');
+
 const requiredStr = { type: String, required: true, trim: true };
 const requiredNum = { type: Number, required: true };
 const requiredBool = { type: Boolean, required: true };
 const currentTime = { type: Date, default: Date.now };
 const requiredDate = { type: Date, required: true };
+const requiredMongoId = { type: mongoose.Types.ObjectId, required: true };
 
 module.exports = {
   requiredStr,
@@ -10,4 +13,5 @@ module.exports = {
   requiredBool,
   currentTime,
   requiredDate,
+  requiredMongoId,
 };
